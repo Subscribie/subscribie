958029812c2e3596c476fb79f80bcd3b6247b5c7
Christopher Simpson <enquiries@karmacomputing.co.uk>
2017-11-06 14:30:22 +0000

    added parsing of broadband checking response from bt wholesale

--- shortly/shortly.py
+++ shortly/shortly.py
@@ -1,12 +1,14 @@
 import os
 import redis
 import urlparse
+import requests
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
+from bs4 import BeautifulSoup
 
 class Shortly(object):
     def __init__(self, config):
@@ -35,15 +37,44 @@ class Shortly(object):
 
     def on_new_url(self,request):
         error = None
-        url = ''
+        result = ''
         if request.method == 'POST':
-            url = request.form['url']
-            if not is_valid_url(url):
-                error = 'Please enter a valid URL'
+            #url = request.form['url']
+            if not is_valid_lookup(request.form):
+                error = 'Please enter a valid request'
             else:
-                short_id = self.insert_url(url)
-                return redirect('/%s+' % short_id)
-        return self.render_template('new_url.html', error=error, url=url)
+                r = requests.post('https://www.dslchecker.bt.com/adsl/ADSLChecker.AddressOutput', 
+                                 data = {'buildingnumber': request.form['buildingnumber'],
+                                       'PostCode': request.form['PostCode']})
+                result = r.text
+                soup = BeautifulSoup(r.text, 'html.parser')
+                soup.prettify()
+                soup.find_all('span')
+                result = {}
+                result['VDSL Range A'] = {'Downstream': {'high':'', 'low':''},'Upstream': {'high':'', 'low':''}}
+                result['WBC ADSL 2+'] = {'Downstream': '','Upstream': ''}
+
+		for span in soup.find_all('span'):
+		    if "VDSL Range A (Clean)" in span:
+			for index, child in enumerate(span.parent.parent.children):
+			    #print index, child
+			    if index == 3:
+				result['VDSL Range A']['Downstream']['high'] = child.text
+			    if index == 5:
+				result['VDSL Range A']['Downstream']['low'] = child.text
+			    if index == 7:
+			       result['VDSL Range A']['Upstream']['high'] = child.text
+			    if index == 9:
+			       result['VDSL Range A']['Upstream']['low'] = child.text
+		    if "WBC ADSL 2+" in span:
+			for index, child in enumerate(span.parent.parent.children):
+			    print index, child
+			    if index == 3:
+				result['WBC ADSL 2+']['Downstream'] = child.text
+			    if index == 5:
+				result['WBC ADSL 2+']['Upstream'] = child.text
+                return self.render_template('result.html', result=result)
+        return self.render_template('new_url.html', error=error)
 
 
 
@@ -104,9 +135,8 @@ def create_app(redis_host='localhost', redis_port=6379, with_static=True):
         })
     return app
 
-def is_valid_url(url):
-    parts = urlparse.urlparse(url)
-    return parts.scheme in ('http', 'https')
+def is_valid_lookup(form):
+    return True
 
 def base36_encode(number):
     assert number >= 0, 'positive integer required'
@@ -121,6 +151,6 @@ def base36_encode(number):
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = create_app()
-    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True, ssl_context='adhoc')
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
 
 
