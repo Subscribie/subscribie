5d820ab37b6eee64372f7931b29826779b97a4c1
Christopher Simpson <enquiries@karmacomputing.co.uk>
2017-11-06 12:53:45 +0000

    adhoc ssl for localdev

--- shortly/shortly.py
+++ shortly/shortly.py
@@ -121,6 +121,6 @@ def base36_encode(number):
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = create_app()
-    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True, ssl_context='adhoc')
 
 
