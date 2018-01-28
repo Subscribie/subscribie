2d86d973eaae79fd8262adb7244c35da52f1f24a
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-01-28 00:36:50 +0000

    Implemented rest penguin_rest; Import from penguin_rest import Rest. Penguin Rest will take the entity (content-type) being POSTed to, and format the payload according to Drupal's REST payload requirements @see format_fields in Rest.py

---
+++ shortly/penguin_rest/Rest.py
@@ -0,0 +1,53 @@
+import requests
+import os
+import json
+
+def post(fields='', entity=''):
+    print "Posting to penguin!"
+
+    #Get CSRF token
+    token = str(requests.get(os.getenv('penguin_url') + '/session/token').text)
+
+    endpoint = os.getenv('penguin_url') + "/node?_format=hal_json"
+    entity_href = ''.join(['"',os.getenv('penguin_url'), '/rest/type/node/', entity,'"'])
+
+    def format_fields(fields):
+	fields_formatted = {}
+	for index, value in enumerate(fields):
+	    fields_formatted[value] = [{"value": fields[value]}]
+	#Convert to json
+	fields_formatted = json.dumps(fields_formatted)
+	#Remove outer brackets and return
+	return fields_formatted[1:-1]
+
+    #Set all required headers
+    headers = {'Content-Type':'application/hal+json',
+	'X-CSRF-Token':token
+    }
+
+    #Include all fields required by the content type
+    payload  = '''
+    {
+    "_links": {
+    "type": {
+    "href": '''
+    payload = payload + entity_href
+    payload = payload + '''
+    }
+    },'''
+    payload = payload + format_fields(fields) + '''
+    }'''
+
+    #Post the new node (a Contact) to the endpoint.
+    user = os.getenv('rest_user')
+    password = os.getenv('rest_password')
+    r = requests.post(endpoint, data=payload, headers=headers, auth=(user,password))
+
+    #Check was a success 
+    if r.status_code == 201:
+	print "Success"
+        print r.text
+    else:
+	print "Fail"
+	print r.status_code
+        print r.text
