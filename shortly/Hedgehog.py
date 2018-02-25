382d0e768fba28c5144dfde04b5e07a303f5f12b
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-02-25 19:31:28 +0000

    removed broadband avalability checker logic from Hedgehog to make more generic when cloning for other services. Services like an avalability checker could be added as standard python modules, or using Flasks built-in "bluerpint" pattern: http://flask.pocoo.org/docs/0.12/blueprints/

--- shortly/Hedgehog.py
+++ shortly/Hedgehog.py
@@ -13,7 +13,9 @@ from jamla import Jamla
 import sendgrid
 from sendgrid.helpers.mail import *
 from flask import Flask, render_template, session, redirect, url_for, escape, request
+import jinja2
 import datetime
+
 
 alphanum = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXYZ0123456789"
 
@@ -26,151 +28,17 @@ with app.app_context():
     if jamla is None:
         jamla = Jamla.load(app.config['JAMLA_PATH'])
 
-@app.route('/', methods=['POST'])
-def result():
-    buildingnumber = request.form['buildingnumber']
-    PostCode = request.form['PostCode']
-    now = datetime.datetime.now()
-    prettyTime = datetime.datetime.now().strftime("%H:%M %D")
-    sid = request.cookies.get('karma_cookie')
-    print app.config['DB_FULL_PATH']
-    con = sqlite3.connect(app.config['DB_FULL_PATH'])
-    cur = con.cursor()
-    cur.execute("INSERT INTO lookups VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (sid, now, buildingnumber, '', '', '', '', PostCode))
-    con.commit()
-    cur.execute("SELECT * FROM lookups")
-    print cur.fetchone()
-    con.close()
-    canADSL = False
-    canFibre = False
-    nophone = False
-    if not is_valid_lookup(request.form):
-        error = 'Please enter a valid request'
-    else:
-        r = requests.post('https://www.dslchecker.bt.com/adsl/ADSLChecker.AddressOutput',
-                         data = {'buildingnumber': request.form['buildingnumber'],
-                               'postCode': request.form['PostCode']})
-        result = r.text
-        soup = BeautifulSoup(r.text, 'html.parser')
-        soup.prettify()
-        soup.find_all('span')
-        result = {}
-        result['VDSL Range A'] = {'Downstream': {'high':'', 'low':''},'Upstream': {'high':'', 'low':''}}
-        result['WBC ADSL 2+'] = {'Downstream': '','Upstream': ''}
-        result['WBC ADSL 2+ Annex M'] = {'Downstream': '','Upstream': ''}
-        result['ADSL Max'] = {'Downstream': '','Upstream': ''}
-
-
-        for span in soup.find_all('span'):
-            if "VDSL Range A (Clean)" in span:
-                for index, child in enumerate(span.parent.parent.children):
-                    #print index, child
-                    if index == 3:
-                        result['VDSL Range A']['Downstream']['high'] = child.text
-                        try:
-                            float(child.text)
-                            canFibre = True
-                        except ValueError as verr:
-                            canFibre = False
-                    if index == 5:
-                        result['VDSL Range A']['Downstream']['low'] = child.text
-                        try:
-                            float(child.text)
-                            canFibre = True
-                        except ValueError as verr:
-                            canFibre = False
-                    if index == 7:
-                       result['VDSL Range A']['Upstream']['high'] = child.text
-                    if index == 9:
-                       result['VDSL Range A']['Upstream']['low'] = child.text
-
-            if "WBC ADSL 2+" in span:
-                for index, child in enumerate(span.parent.parent.children):
-                    print index, child
-                    if index == 3:
-                        result['WBC ADSL 2+']['Downstream'] = child.text.replace('Up to ','')
-                        try:
-                            float(child.text.replace('Up to ',''))
-                            canADSL = True
-                        except ValueError as verr:
-                            canADSL = False
-                    if index == 5:
-                        result['WBC ADSL 2+']['Upstream'] = child.text.replace('Up to ','')
-                        if result['WBC ADSL 2+']['Upstream'].find("--") != -1:
-                                result['WBC ADSL 2+']['Upstream'] = ""
-
-            if "WBC ADSL 2+ Annex M" in span:
-                for index, child in enumerate(span.parent.parent.children):
-                    print index, child
-                    if index == 3:
-                        result['WBC ADSL 2+ Annex M']['Downstream'] = child.text.replace('Up to ','')
-                        try:
-                            float(child.text.replace('Up to ',''))
-                            canADSL = True
-                        except ValueError as verr:
-                            canADSL = False
-                    if index == 5:
-                        result['WBC ADSL 2+ Annex M']['Upstream'] = child.text.replace('Up to ','')
-                        if result['WBC ADSL 2+ Annex M']['Upstream'].find("--") != -1:
-                                result['WBC ADSL 2+ Annex M']['Upstream'] = ""
-
-            if "ADSL Max" in span:
-                for index, child in enumerate(span.parent.parent.children):
-                    print index, child
-                    if index == 3:
-                        result['ADSL Max']['Downstream'] = child.text.replace('Up to ','')
-                        try:
-                            float(child.text.replace('Up to ',''))
-                            canADSL = True
-                        except ValueError as verr:
-                            canADSL = False
-                    if index == 5:
-                        result['ADSL Max']['Upstream'] = child.text.replace('Up to ','')
-                        if result['ADSL Max']['Upstream'].find("--") != -1:
-                                result['ADSL Max']['Upstream'] = ""
-
-        try:
-            if request.cookies.get('nophone'):
-                nophone=True
-        except  KeyError:
-            pass
-        uptoSpeedFibre = result['VDSL Range A']['Downstream']['high']
-        uptoSpeedADSL = result['WBC ADSL 2+']['Downstream']
-
-        fields = {'title':time.time(),
-              'field_building_number':request.form['buildingnumber'],
-              'field_postcode_availability':request.form['PostCode'],
-              'field_vdsl_a_clean_mbps_high_dl':result['VDSL Range A']['Downstream']['high'],
-              'field_vdsl_a_clean_mbps_low_dl':result['VDSL Range A']['Downstream']['low'],
-              'field_vdsl_a_clean_mbps_high_ul':result['VDSL Range A']['Upstream']['high'],
-              'field_vdsl_a_clean_mbps_low_ul': result['VDSL Range A']['Upstream']['low'],
-              'field_adsl_2_downstream': result['WBC ADSL 2+']['Downstream'],
-              'field_adsl_2_upstream': result['WBC ADSL 2+']['Upstream'],
-              'field_adsl_2_annex_m_downstream': result['WBC ADSL 2+ Annex M']['Downstream'],
-              'field_adsl_2_annex_m_upstream': result['WBC ADSL 2+ Annex M']['Upstream'],
-              'field_adsl_max_downstream': result['ADSL Max']['Downstream'],
-              'field_adsl_max_upstream': result['ADSL Max']['Upstream']}
-        Rest.post(entity='broadband_availability_lookup', fields=fields)
-
-    return render_template('result.html', jamla=jamla, result=result, canADSL=canADSL, canFibre=canFibre, buildingnumber=buildingnumber, postCode=PostCode, now=prettyTime, nophone=nophone)
-
+my_loader = jinja2.ChoiceLoader([
+            jinja2.FileSystemLoader([jamla['site']['template_folder']]),
+            app.jinja_loader,
+        ])
+app.jinja_loader = my_loader
 
 @app.route('/', methods=['GET'])
-def start():
+def choose():
     session['sid'] = b64encode(''.join([alphanum[random.randint(0, len(alphanum) - 1)] for _ in range(0, 24)])).decode('utf-8')
-    error = None
-    nophone = False
-    try:
-        if request.cookies.get('nophone'):
-            nophone=True
-    except  KeyError:
-        pass
-    result = ''
-    return render_template('start.html', error=error, cheese=True, nophone=nophone)
 
-def is_valid_lookup(form):
-    return True
-
+    return render_template('choose.html', jamla=jamla)
 
 @app.route('/new_customer', methods=['GET'])
 def new_customer():
@@ -284,27 +152,16 @@ def on_complete_mandate():
     customerAddress = row[4] + ", " + row[5] + ", " + row[6]
     customerEmail = row[7]
     customerPhone = row[8]
-    broadbandPackage = row[9]
+    chosenPackage = row[9]
     customerExistingLine = row[10]
     customerExistingNumber = row[11]
     currentDate = datetime.datetime.now()
     goLive = currentDate + datetime.timedelta(days = 15)
 
-    if broadbandPackage == "adsl":
-        broadbandPackage = "ADSL 2+"
-        contractExpiry = goLive + datetime.timedelta(days = 90)
-        monthlyCost = "34.99"
-    elif broadbandPackage == "fibre":
-        broadbandPackage = "FTTC 40:10"
-        contractExpiry = goLive + datetime.timedelta(days = 365)
-        monthlyCost = "41.99"
-    elif broadbandPackage == "fibre_plus":
-        broadbandPackage = "FTTC 80:20"
-        contractExpiry = goLive + datetime.timedelta(days = 365)
-        monthlyCost = "41.99"
-
-    contractExpiry = contractExpiry.strftime('%d/%m/%Y') + "*"
-    goLive = goLive.strftime('%d/%m/%Y')
+    #TODO loop over Jamla items vs chosenPackage to work out contractExpiry, monthlycost, lead_time
+    contractExpiry = goLive + datetime.timedelta(days = 365)
+    monthlyCost = "As quoted"
+    goLive = "ASAP"
 
     ## ADMIN
     sg = sendgrid.SendGridAPIClient(apikey=app.config['SENDGRID_API_KEY'])
@@ -317,37 +174,29 @@ def on_complete_mandate():
     mail.personalizations[0].add_substitution(Substitution("-customerPhone-", customerPhone))
     mail.personalizations[0].add_substitution(Substitution("-customerAddress-", customerAddress))
     mail.personalizations[0].add_substitution(Substitution("-customerEmail-", customerEmail))
-    mail.personalizations[0].add_substitution(Substitution("-broadbandPackage-", broadbandPackage))
+    mail.personalizations[0].add_substitution(Substitution("-broadbandPackage-", chosenPackage))
     mail.personalizations[0].add_substitution(Substitution("-customerExistingLine-", customerExistingLine))
     mail.personalizations[0].add_substitution(Substitution("-customerExistingNumber-", customerExistingNumber))
     mail.template_id = "8b49f623-9368-4cf6-94c1-53cc2f429b9b"
     try:
         response = sg.client.mail.send.post(request_body=mail.get())
-    except urllib.HTTPError as e:
-        print (e.read())
-        exit()
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+    except Exception:
+        pass
 
     ## CUSTOMER
     from_email = Email("broadband@karmacomputing.co.uk", "Karma Broadband Team")
     to_email = Email(customerEmail)
     mail = Mail(from_email, subject, to_email, content)
     mail.personalizations[0].add_substitution(Substitution("-name-", customerName))
-    mail.personalizations[0].add_substitution(Substitution("-package-", broadbandPackage))
+    mail.personalizations[0].add_substitution(Substitution("-package-", chosenPackage))
     mail.personalizations[0].add_substitution(Substitution("-contractExpiry-", contractExpiry))
     mail.personalizations[0].add_substitution(Substitution("-goLive-", goLive))
     mail.personalizations[0].add_substitution(Substitution("-monthlyCost-", monthlyCost))
     mail.template_id = "0c383660-2801-4448-b3cf-9bb608de9ec7"
     try:
         response = sg.client.mail.send.post(request_body=mail.get())
-    except urllib.HTTPError as e:
-        print (e.read())
-        exit()
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+    except Exception:
+        pass
 
     # Display a confirmation page to the customer, telling them
     # their Direct Debit has been set up. You could build your own,
