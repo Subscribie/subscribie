09e38f83ec4a87a44d6218062b631dd4101b2036
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-03-16 23:06:32 +0000

    wip refactor of Rest.py Post method

--- shortly/penguin_rest/Rest.py
+++ shortly/penguin_rest/Rest.py
@@ -3,30 +3,38 @@ import os
 import json
 from flask import current_app as app
 
-def post(fields='', entity=''):
-    print "Posting to penguin!"
-
+def get_csrf_token():
     #Get CSRF token
     token = str(requests.get(app.config['PENGUIN_URL'] + '/session/token').text)
+    return token
+
+def get_headers():
+    #Set all required headers
+    headers = {'Content-Type':'application/hal+json',
+	'X-CSRF-Token':get_csrf_token()
+    }
+    return headers
+
+
+def format_fields(fields):
+    fields_formatted = {}
+    for index, value in enumerate(fields):
+        fields_formatted[value] = [{"value": fields[value]}]
+    #Convert to json
+    fields_formatted = json.dumps(fields_formatted)
+    #Remove outer brackets and return
+    return fields_formatted[1:-1]
+
+def post(fields='', entity=''):
+    print "Posting to penguin!"
 
     endpoint = app.config['PENGUIN_URL'] + "/node?_format=hal_json"
     entity_href = ''.join(['"', app.config['PENGUIN_URL'], '/rest/type/node/', entity,'"'])
     # Replace https with http for entity href only (endpoint remains https)
     entity_href = entity_href.replace('https','http')
 
-    def format_fields(fields):
-	fields_formatted = {}
-	for index, value in enumerate(fields):
-	    fields_formatted[value] = [{"value": fields[value]}]
-	#Convert to json
-	fields_formatted = json.dumps(fields_formatted)
-	#Remove outer brackets and return
-	return fields_formatted[1:-1]
-
     #Set all required headers
-    headers = {'Content-Type':'application/hal+json',
-	'X-CSRF-Token':token
-    }
+    headers = get_headers()
 
     #Include all fields required by the content type
     payload  = '''
