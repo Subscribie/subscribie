177a63e691e85bdb12faca75db47268833df6cf8
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-03-16 23:39:54 +0000

    Implemented basic Rest.patch() method for basic data types. TODO add entity reference patch capability.
    Example patch request:
    
        title = "changed"
        fields = {
    	'title':title
        }
        Rest.patch(nid=123, entity='partner', fields=fields)

--- shortly/penguin_rest/Rest.py
+++ shortly/penguin_rest/Rest.py
@@ -65,5 +65,43 @@ def post(fields='', entity=''):
         print r.text
         return r
 
-def patch():
-    pass
+def patch(nid, entity, fields):
+    print "Patching to Penguin!"
+
+    endpoint = app.config['PENGUIN_URL'] + "/node/" + str(nid) + "?_format=hal_json"
+    entity_href = ''.join(['"', app.config['PENGUIN_URL'], '/rest/type/node/', entity,'"'])
+    # Replace https with http for entity href only (endpoint remains https)
+    entity_href = entity_href.replace('https','http')
+
+    #Set all required headers
+    headers = get_headers()
+
+    #Include all fields required by the content type
+    payload  = '''
+    {
+    "_links": {
+    "type": {
+    "href": '''
+    payload = payload + entity_href
+    payload = payload + '''
+    }
+    },'''
+    payload = payload + format_fields(fields) + '''
+    }'''
+
+    #Post the new node (a Contact) to the endpoint.
+    user = app.config['REST_USER']
+    password = app.config['REST_PASSWORD']
+    r = requests.patch(endpoint, data=payload, headers=headers, auth=(user,password))
+
+    #Check was a success 
+    if r.status_code == 200:
+	print "Patch Successful"
+	print r.status_code
+        print r.text
+        return r
+    else:
+	print "Patch Fail"
+	print r.status_code
+        print r.text
+        return r
