648e6fab16fb170ec3b78073b17e219896c0fa2b
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-03-16 16:58:00 +0000

    wip getting post new partner reponse object

--- shortly/penguin_rest/Decorators.py
+++ shortly/penguin_rest/Decorators.py
@@ -1,6 +1,7 @@
 from penguin_rest import Rest
 from flask import g, request, redirect, url_for
 from functools import wraps
+import json
 
 def create_customer(f):
     @wraps(f)
@@ -12,9 +13,13 @@ def create_customer(f):
                 'title':title,
                 'field_customer': 1
             }
-            Rest.post(entity='partner', fields=fields)
-        except:
+            r = Rest.post(entity='partner', fields=fields)
+            resp = json.loads(r.text)
+            partner_nid = resp['nid'][0]['value']
+            print "Partner node id is: " + str(partner_nid)
+            print r
+        except Exception as e:
             print "Failed creating partner of customer type.."
-            pass
+            print e
         return f(*args, **kwargs)
     return decorated_function
