98a87c0ce2931bf651fd6b85d382830c0a52a815
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-04-24 11:07:11 +0100

    wip flask_login example (no views) TODO: teach flask_login to use our own token login

--- shortly/Hedgehog.py
+++ shortly/Hedgehog.py
@@ -56,6 +56,30 @@ with app.app_context():
 
         login_manager = flask_login.LoginManager()
         login_manager.init_app(app)
+        # Mock database
+        users = {'foo@bar.tld': {'password':'secret'}}
+
+        class User(flask_login.UserMixin):
+            pass
+
+        @login_manager.user_loader
+        def user_loader(email):
+            if email not in users:
+                return
+            user = User()
+            user.id = email
+            return user
+
+        @login_manager.request_loader
+        def request_loader(request):
+            email = request.form.get('email')
+            if email not in users:
+                return
+            user = User()
+            user.id = email
+
+            user.is_authenticated = request.form['password'] == users['email']['password']
+            return user
 
 	@app.route('/', methods=['GET'])
 	def choose():
