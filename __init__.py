f6ebce219ae1e93f46f19b4255cd91cd19574344
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-05-06 15:41:36 +0100

    jamla.py now defines Jamla as a class

--- __init__.py
+++ __init__.py
@@ -12,6 +12,6 @@
 __version__ = '0.0.1'
 
 # jamla
-import jamla
+from .jamla import Jamla
 # the signals
 from .signals import journey_complete
--- jamla.py
+++ jamla.py
@@ -1,21 +1,23 @@
 import yaml
 
-def load(src = 'example.yaml'):
-    with open(src, 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-    for section in cfg:
-        print (section)
-    return cfg
+class Jamla():
 
-def get_primary_icons(jamla=None):
-    """ Returns list of all static file paths """
-    if jamla is None:
-        raise Exception("get_primary_icons expects a jamla object")
-    icons = []
-    for item in jamla['items']:
-        if item['primary_icon']['src']:
-            icons.append(item['primary_icon']['src'])
-    return icons
+    def load(self, src='example.yaml'):
+        with open(src, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+        for section in cfg:
+            print (section)
+        return cfg
+
+    def get_primary_icons(self, jamla=None):
+        """ Returns list of all static file paths """
+        if jamla is None:
+            raise Exception("get_primary_icons expects a jamla object")
+        icons = []
+        for item in jamla['items']:
+            if item['primary_icon']['src']:
+                icons.append(item['primary_icon']['src'])
+        return icons
 
 
 
