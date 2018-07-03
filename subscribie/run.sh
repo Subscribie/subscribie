2b45ba9bf2fe502112b0f615c697916f62e0c07e
Christopher Simpson <enquiries@karmacomputing.co.uk>
2018-07-03 10:42:00 +0100

    rename run script

--- subscribie/run.sh
+++ subscribie/run.sh
@@ -6,7 +6,7 @@ then
     echo "In production usin mod_wsgi, reloading apache2"
     sudo service apache2 reload
 else
-    echo "hedgehog is in development mode. Starting simple python server..."
+    echo "subscribie is in development mode. Starting simple python server..."
     export FLASK_DEBUG=1
     export HEDGEHOG_ENV=./.env
     export FLASK_APP=__init__.py
