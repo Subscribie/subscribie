b7a5aa478d05a65cee1e923722fac446cb6bf867
chrisjsimpson <enquiries@karmacomputing.co.uk>
2018-07-04 17:52:03 +0100

    updated setuptools setup.py with required packages

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='hedgehog',
-    packages=['hedgehog'],
+setuptools.setup(
+    name='Subscribie',
+    version="0.0.1",
+    author="Christopher Simpson",
+    author_email="chris@karmacomputing.co.uk",
+    desciption="Recurring subscription management and billing",
+    packages=setuptools.find_packages(),
     include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
     install_requires=[
         'flask',
+        'flask_cors',
+        'flask_login', 
+        'flask_uploads',
+        'flask_wtf',
+        'requests',
+        'blinker',
+        'oauth2client',
+        'pyyaml',
+        'stripe',
+        'gocardless_pro',
     ],
 )
