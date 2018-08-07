5f463646c2547f31060c5a2aeb79b6f25c636f65
chrisjsimpson <chris@karmacomputing.co.uk>
2018-08-07 16:13:32 +0100

    corrected config DATABASE to DB_FULL_PATH

--- subscribie/db.py
+++ subscribie/db.py
@@ -15,7 +15,7 @@ def get_jamla():
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(current_app.config["DATABASE"],
+        g.db = sqlite3.connect(current_app.config["DB_FULL_PATH"],
                                detect_types=sqlite3.PARSE_DECLTYPES)
         g.db.row_factory = sqlite3.Row
     return g.db
