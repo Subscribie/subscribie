d4a1af1444f2c17f82b41004b016be7026022473
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-11 21:25:48 +0000

    require at least version 0.9.1 of Flask-Mail

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         'flask_login', 
         'flask_uploads',
         'flask_wtf',
-        'Flask-Mail==0.9.1',
+        'Flask-Mail>=0.9.1',
         'requests',
         'blinker',
         'oauth2client',
