733812322df6759925cafa6e3391fc288147bf35
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-20 22:26:23 +0100

    wip consume new site request from couchdb, generate manifest and deploy to kubernetes

--- kubernetes/cronjob.py
+++ kubernetes/cronjob.py
@@ -1,31 +1,117 @@
 import requests
 import json
-HOST = "http://admin:password@127.0.0.1:5984/jamlas/_all_docs"
-# Get a document
-'''
-  - at random , it dosent matter.
-'''
-requests.get(HOST)
+import random
+import re
+import subprocess
+from kubernetes import client, config
+import yaml
+import tempfile
+
+
+
+
+HOST = "http://admin:password@127.0.0.1:5984/jamlas"
+
+def getDoc(docId):
+  ''' Return doc from Couchdb 
+  docId: Document id
+  returns: Complete doc (excluding attachments)
+  '''
+  req = requests.get(HOST + '/' + docId)
+  resp = json.loads(req.text)
+  return resp
+def generateManifest(docId):
+  ''' Generate Kubernetes manifest yaml from CouchDB document
+  the attached jamla document is to be injected into the
+  subscribie container, along with any environment vars
+  needed. '''
+  pass
+  # Generate manifest
+  doc = getDoc(docId)
+  try:
+    siteName = re.sub(r'\W+', '', doc['company']['name']).lower()
+    deploymentName = siteName
+    
+    manifest = {
+            'apiVersion': 'apps/v1', 
+            'kind': 'Deployment', 
+            'metadata': {
+              'name': deploymentName, 
+              'labels': {
+                'subscribie': 'site',
+                'sitename': siteName,
+                'docid': siteName # Doc id is the sitename
+              }
+            }, 
+            'spec': {
+              'replicas': 2, 
+              'selector': {
+                'matchLabels': {
+       
+                  'subscribie': siteName}
+              }, 
+            'template': {
+              'metadata': {
+                'labels': {
+                  'app': 'subscribie', 
+                  'subscribie': siteName}
+              }, 
+              'spec': {
+                'containers': [{
+                  'name': 'subscribie', 
+                  'image': 'subscribie/subscribie:v0.0.1', 
+                  'imagePullPolicy': 'Always', 
+                  'ports': [{'containerPort': 9090}], 
+                  'env': [{
+                    'name': 'EXAMPLE', 
+                    'value': 'example_value'
+                  }]
+                }]
+              }
+            }
+            }
+        }
+    return manifest
+  except KeyError:
+    print("Error could not parse site jamla. (KeyError)")
+  '''
+    - storage? Ceph? No. Not ready for it. Possibly CouchDB, 
+      just give clients an object store.
+  '''
+
+def deployManifest(manifest):
+  config.load_kube_config()
+  deployment = manitest
+  v1 = client.AppsV1Api()
+  rsp = v1.create_namespaced_deployment(
+          body=deployment, namespace="default")
+  import pdb;pdb.set_trace()
+  return rsp
+
+req = requests.get(HOST + '/_all_docs')
+resp = json.loads(req.text)
+try:
+  # Get a (TODO non-deployed) document at random , it dosent matter.
+  docRow = random.choice(resp['rows'])
+  manitest = generateManifest(docRow['id'])
+  response = deployManifest(manitest)
+except IndexError:
+  print("Do documents left to process")
+
+  
+
 
 
   
-# Generate manifest
-'''
-  - label with document id from couchdb
-  - deployment? Probably.
-  - just pod?
-  - storage? Ceph?
-'''
 # Submit manitest to kubernestes
-'''
-  - kubectl apply -f <>.yaml
-'''
 
 # Verify deployment is OK
 '''
   - if deployment is OK
     - Delete document from couchdb
       - or mark as complete? NO this leaves mess. Just force client to retry
+      - No. Mark it as deployed by updating the document. Allow attatchments for
+        assets. Consider one couchbase database per user (DO IT)
 '''
 # Report back to client this is sucess
 '''
