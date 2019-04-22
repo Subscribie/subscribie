5ca6b6f13449b38e5603e669337435a381e46240
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-22 15:47:42 +0100

    fallback to simple defaul logging

--- subscribie/__init__.py
+++ subscribie/__init__.py
@@ -9,11 +9,8 @@
 """
 from os import path
 import logging
-import logging.config
-log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
-logging.config.fileConfig(log_file_path)
 # Create logger
-logger = logging.getLogger('subscribie')
+logger = logging
 import os
 from os import environ
 import sys
