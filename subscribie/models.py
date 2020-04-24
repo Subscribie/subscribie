fe2e17158167debe1aded13b76e5e1fd0aa63832
chrisjsimpson <chris@karmacomputing.co.uk>
2020-04-24 21:38:45 +0100

    wip #138 subscriptions model and migrations

--- subscribie/models.py
+++ subscribie/models.py
@@ -1,5 +1,8 @@
 from . import database
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 from datetime import datetime
+from uuid import uuid4
 
 class User(database.Model):
     __tablename__ = 'user'
@@ -15,6 +18,7 @@ class User(database.Model):
 class Person(database.Model):
     __tablename__ = 'person'
     id = database.Column(database.Integer(), primary_key=True)
+    uuid = database.Column(database.String(), default=str(uuid4()))
     sid = database.Column(database.String())
     ts = database.Column(database.DateTime, default=datetime.utcnow)
     given_name = database.Column(database.String())
@@ -24,6 +28,16 @@ class Person(database.Model):
     postal_code = database.Column(database.String())
     email = database.Column(database.String())
     mobile = database.Column(database.String())
+    subscriptions = relationship("Subscription", back_populates="person")
 
     def __repr__(self):
         return '<Person {}>'.format(self.given_name)
+
+class Subscription(database.Model):
+    __tablename__ = 'subscription'
+    id = database.Column(database.Integer(), primary_key=True)
+    uuid = database.Column(database.String(), default=str(uuid4()))
+    sku_uuid = database.Column(database.String())
+    person_id = database.Column(database.Integer(), ForeignKey('person.id'))
+    person = relationship("Person", back_populates="subscriptions")
+
