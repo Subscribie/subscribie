e5858200748e54ac5c27a2fc82dcbada7f8f2f74
chrisjsimpson <chris@karmacomputing.co.uk>
2020-08-05 19:17:19 +0100

    wip choice-group and options#

--- subscribie/blueprints/admin/__init__.py
+++ subscribie/blueprints/admin/__init__.py
@@ -19,6 +19,7 @@ from subscribie import (
     GoogleTagManagerConnectForm,
     PlansForm,
     ItemForm,
+    ChoiceGroupForm,
     jsonify,
     TawkConnectForm,
     database, User, Person, Subscription, SubscriptionNote, Company, Item,
@@ -36,7 +37,7 @@ import subprocess
 import uuid
 from sqlalchemy import asc, desc
 from datetime import datetime
-from subscribie.models import Transaction
+from subscribie.models import ChoiceGroup, Transaction
 
 admin = Blueprint(
     "admin", __name__, template_folder="templates", static_folder="static"
@@ -353,10 +354,16 @@ def list_items():
     items = Item.query.all()
     return render_template("admin/item/list_items.html", items=items)
 
-@admin.route("/edit-item/<id>", methods=["GET"])
+@admin.route("/edit-item/<id>", methods=["GET", "POST"])
 @login_required
 def edit_item(id):
     item = Item.query.get(id)
+    form = ItemForm()
+    if request.method == 'POST':
+        item.title = request.form['title']
+        item.description = request.form['description']
+        database.session.commit()
+        flash("Item updated")
     return render_template("admin/item/edit_item.html", item=item)
 
 @admin.route("/delete-item/<id>", methods=["GET"])
@@ -376,6 +383,57 @@ def delete_item(id):
     database.session.commit()
     flash("Item deleted")
     return redirect(url_for('admin.list_items'))
+
+@admin.route("/add-choice-group", methods=["GET", "POST"])
+@login_required
+def add_choice_group():
+    form = ChoiceGroupForm()
+    if form.validate_on_submit():
+        choice_group = ChoiceGroup()
+        choice_group.title = request.form['title']
+        choice_group.description = request.form['description']
+        database.session.add(choice_group)
+        database.session.commit()
+        flash("Added new choice group")
+        return redirect(url_for('admin.list_choice_groups'))
+    
+    return render_template("admin/choice_group/add_choice_group.html", form=form)
+
+@admin.route("/list-choice-groups", methods=["GET", "POST"])
+@login_required
+def list_choice_groups():
+    choice_groups = ChoiceGroup.query.all()
+    return render_template("admin/choice_group/list_choice_groups.html", choice_groups=choice_groups)
+
+@admin.route("/edit-choice-group/<id>", methods=["GET", "POST"])
+@login_required
+def edit_choice_group(id):
+    choice_group = ChoiceGroup.query.get(id)
+    if request.method == 'POST':
+        choice_group.title = request.form['title']
+        choice_group.description = request.form['description']
+        database.session.commit()
+        flash("Choice group updated")
+    return render_template("admin/choice_group/edit_choice_group.html", choice_group=choice_group)
+
+@admin.route("/delete-choice-group/<id>", methods=["GET"])
+@login_required
+def delete_choice_group(id):
+    if "confirm" in request.args:
+        choice_groups = ChoiceGroup.query.all()
+
+        confirm = False
+        return render_template(
+            "admin/choice_group/list_choice_groups.html",
+            choice_groups=choice_groups,
+            choice_group=ChoiceGroup.query.get(id),
+            confirm=False,
+        )
+    choice_group = ChoiceGroup.query.get(id)
+    database.session.delete(choice_group)
+    database.session.commit()
+    flash("Choice group deleted")
+    return redirect(url_for('admin.list_choice_groups'))
 
 @admin.route("/add", methods=["GET", "POST"])
 @login_required
