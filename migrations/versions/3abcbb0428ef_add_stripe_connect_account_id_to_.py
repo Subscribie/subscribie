226f9ebc23581e54364b7d9233e084e1ac76aeac
chrisjsimpson <chris@karmacomputing.co.uk>
2020-10-01 09:43:35 +0100

    add migration from 287-automate-stripe-connection branch stripe_connect_account_id

---
+++ migrations/versions/3abcbb0428ef_add_stripe_connect_account_id_to_.py
@@ -0,0 +1,25 @@
+"""add stripe_connect_account_id to PaymentProvider model
+
+Revision ID: 3abcbb0428ef
+Revises: 790aae5a7013
+Create Date: 2020-09-23 17:17:30.127947
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3abcbb0428ef'
+down_revision = '790aae5a7013'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("payment_provider") as batch_op:
+        batch_op.add_column(sa.Column('stripe_connect_account_id', sa.String(), nullable=True))
+
+
+def downgrade():
+    pass
