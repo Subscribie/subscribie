e43dbd8827552e2bf15006c3e471dfcf26063591
chrisjsimpson <chris@karmacomputing.co.uk>
2020-11-02 16:40:14 +0000

    wip #315 add back stripe test mode columns

---
+++ migrations/versions/ee8e62e05b40_add_back_stripe_test_mode_colums.py
@@ -0,0 +1,30 @@
+"""add back stripe test mode colums
+
+Revision ID: ee8e62e05b40
+Revises: 686d588c3c29
+Create Date: 2020-11-02 16:38:32.337427
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "ee8e62e05b40"
+down_revision = "686d588c3c29"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("payment_provider") as batch_op:
+        batch_op.add_column(
+            sa.Column("stripe_test_webhook_endpoint_id", sa.String(), nullable=True)
+        )
+        batch_op.add_column(
+            sa.Column("stripe_test_webhook_endpoint_secret", sa.String(), nullable=True)
+        )
+
+
+def downgrade():
+    pass
