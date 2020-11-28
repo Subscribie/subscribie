5fb9326626fe87f7a017b4a28516256ba0d5bda5
chrisjsimpson <chris@karmacomputing.co.uk>
2020-11-28 22:49:43 +0000

    turn of samesite cookie for docker local dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ flask db upgrade
 
 flask initdb
 
-exec uwsgi --http :80 --workers 2 --wsgi-file subscribie.wsgi --touch-chain-reload subscribie.wsgi --chdir /usr/src/app/subscribie/
+exec uwsgi --http :80 --workers 1 --wsgi-file subscribie.wsgi --touch-chain-reload subscribie.wsgi --chdir /usr/src/app/subscribie/
 
