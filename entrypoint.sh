98fd687c1ab5648d47623d93cf0318b2dc106b44
chrisjsimpson <chris@karmacomputing.co.uk>
2020-11-28 17:58:18 +0000

    added docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 export FLASK_APP=subscribie
 export FLASK_DEBUG=1
-flask initdb
+
+#flask initdb
 
 exec uwsgi --http :80 --workers 2 --wsgi-file subscribie.wsgi --touch-chain-reload subscribie.wsgi --chdir /usr/src/app/subscribie/
+
