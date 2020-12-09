eef78b57e74baaeb633d60b9088ff448e721269b
chrisjsimpson <chris@karmacomputing.co.uk>
2020-12-09 22:11:03 +0000

    wip #293 refactored browser tests into modules

---
+++ tests/browser-automated-tests-playwright/tests/test_order_plan_with_only_upfront_charge.js
@@ -0,0 +1,90 @@
+const playwright = require('playwright');
+const assert = require('assert');
+const DEFAULT_TIMEOUT = 10000
+
+/* Test an order can be placed for a plan with only an upfront payment */
+async function test_order_plan_with_only_upfront_charge(browsers) {
+  for (const browserType of browsers) {
+    console.log("test_order_plan_with_only_upfront_charge");
+    const browser = await playwright[browserType].launch({headless: false});
+    const context = await browser.newContext({
+      //...iPhone
+    });
+    context.setDefaultTimeout(DEFAULT_TIMEOUT);
+    const page = await context.newPage();
+
+    // Buy item with subscription & upfront fee
+    await page.goto('http://127.0.0.1:5000/'); // Go to home before selecting product
+    await page.goto('http://127.0.0.1:5000/new_customer?plan=58921f7a-3371-4ccf-aeee-e2b8af5cca3a');
+
+    // Fill in order form
+    await page.fill('#given_name', 'John');
+    await page.fill('#family_name', 'Smith');
+    await page.fill('#email', 'john@example.com');
+    await page.fill('#mobile', '07123456789');
+    await page.fill('#address_line_one', '123 Short Road');
+    await page.fill('#city', 'London');
+    await page.fill('#postcode', 'L01 T3U');
+    await page.screenshot({ path: `new-customer-form-${browserType}.png` });
+    await page.click('.btn-primary');
+    await page.screenshot({ path: `begin-payment-step-${browserType}.png` });
+    // Begin stripe checkout
+    await page.screenshot({ path: `pre-stripe-checkout-${browserType}.png` });
+    await page.click('#checkout-button');
+
+    //Verify first payment is correct (upfront charge + first recuring charge)
+    const first_payment_content = await page.textContent('#ProductSummary-totalAmount');
+    assert(first_payment_content === "£5.66");
+    const upfront_charge_content = await page.textContent('.Text-fontSize--16');
+    assert(upfront_charge_content === "One-Off Soaps");
+
+    // Pay with test card
+    await page.fill('#cardNumber', '4242 4242 4242 4242');
+    await page.fill('#cardExpiry', '04 / 24');
+    await page.fill('#cardCvc', '123');
+    await page.fill('#billingName', 'John Smith');
+    await page.fill('#billingPostalCode', 'LN1 7FH');
+    await page.screenshot({ path: `stripe-checkout-filled-in-${browserType}.png` });
+    await page.click('.SubmitButton');
+  
+    // Verify get to the thank you page order complete
+    context.setDefaultTimeout(30000);
+    const order_complete_content = await page.textContent('.title');
+    assert(order_complete_content === "Order Complete!");
+    await page.screenshot({ path: `order-complete-${browserType}.png` });
+    
+    // Login and verify order appears in admin dashboard
+    // Login
+    await page.goto('http://127.0.0.1:5000/auth/login');
+    await page.fill('#email', 'admin@example.com');
+    await page.fill('#password', 'password');
+    await page.click('#login');
+    await page.screenshot({ path: `logged-in-${browserType}.png` });
+    // Assert logged in OK
+    const content = await page.textContent('.card-title')
+    assert(content === 'Checklist'); // If we see "Checklist", we're logged in to admin
+
+    // Go to My Subscribers page
+    await page.goto('http://127.0.0.1:5000/admin/subscribers')
+    await page.screenshot({ path: `view-subscribers-${browserType}.png` });
+
+    // Verify that subscriber is present in the list
+    const subscriber_email_content = await page.textContent('.subscriber-email');
+    assert(subscriber_email_content === 'john@example.com');
+
+    // Verify that plan is attached to subscriber
+    const subscriber_plan_title_content = await page.textContent('.subscription-title');
+    assert(subscriber_plan_title_content === 'One-Off Soaps');
+
+    // Logout of shop owners admin dashboard
+    await page.goto('http://127.0.0.1:5000/auth/logout');
+    await page.screenshot({ path: `logged-out-${browserType}.png` });
+    // Assert logged out OK
+    const logged_out_content = await page.textContent('.text-center');
+    assert(logged_out_content === "You have logged out");
+ 
+    await browser.close();
+  }
+};
+
+module.exports = test_order_plan_with_only_upfront_charge;
