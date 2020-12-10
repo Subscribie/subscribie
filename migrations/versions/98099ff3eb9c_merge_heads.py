1ecfd04c86f2fc69d7a78945100ec37106931352
chrisjsimpson <chris@karmacomputing.co.uk>
2020-12-10 20:45:43 +0000

    alembic merge heads 75a87d5ab587 9189f7033477

---
+++ migrations/versions/98099ff3eb9c_merge_heads.py
@@ -0,0 +1,21 @@
+"""merge heads
+
+Revision ID: 98099ff3eb9c
+Revises:
+Create Date: 2020-12-10 20:37:39.753780
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '98099ff3eb9c'
+down_revision = ('75a87d5ab587', '9189f7033477')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
