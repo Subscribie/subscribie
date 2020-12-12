636085c9bb19df63e912f80b55e6b17dd76f7b62
chrisjsimpson <chris@karmacomputing.co.uk>
2020-12-12 21:29:28 +0000

    wip #352 shop can announce its strip connect account id to INTERNAL_STRIPE_CONNECT_ACCOUNT_ANNOUNCE_SERVER

--- subscribie/blueprints/admin/__init__.py
+++ subscribie/blueprints/admin/__init__.py
@@ -1089,32 +1089,50 @@ def set_reply_to_email():
 
 @admin.route("/announce-stripe-connect", methods=["GET"])
 def announce_shop_stripe_connect_ids():
-    """The single stripe webhook endpoint needs to know
+    """Accounce this shop's stripe connect account id(s)
+    to the INTERNAL_STRIPE_CONNECT_ACCOUNT_ANNOUNCE_SERVER
+    - stripe_live_connect_account_id
+    - stripe_test_connect_account_id
+    The single stripe webhook endpoint needs to know
     which shops to send events to. Do this this, it needs
     to know the stripe_live_connect_account_id and
     stripe_test_connect_account_id on the payment_provider
-    model, as well as the shop address (flask.host).
+    model, as well as the shop address (flask.request.host_url).
 
     This endpoint (announce_shop_stripe_connect_ids) is called
     via cron for each shop to keep the connect_account_id
-    up to date should they change.
+    up to date should they change. It can also be called manually.
+    It does not matter how many times this is called.
     """
     payment_provider = PaymentProvider.query.first()
     if payment_provider.stripe_live_connect_account_id is not None:
-        # send it
         connect_account_id = payment_provider.stripe_live_connect_account_id
-        r = requests.post(
-            "https://httpbin.org/post", data={"connect_account_id": connect_account_id}
+        requests.post(
+            current_app.config["INTERNAL_STRIPE_CONNECT_ACCOUNT_ANNOUNCE_SERVER"],
+            json={
+                "stripe_connect_account_id": connect_account_id,
+                "live_mode": 1,
+                "site_url": request.host_url,
+            },
         )
     if payment_provider.stripe_test_connect_account_id is not None:
         # send test connect account id
         connect_account_id = payment_provider.stripe_test_connect_account_id
-        post = requests.post(
-            "https://httpbin.org/post", data={"connect_account_id": connect_account_id}
+        requests.post(
+            current_app.config["INTERNAL_STRIPE_CONNECT_ACCOUNT_ANNOUNCE_SERVER"],
+            json={
+                "stripe_connect_account_id": connect_account_id,
+                "live_mode": 0,
+                "site_url": request.host_url,
+            },
         )
-        import pdb
 
-        pdb.set_trace()
+    return (
+        f"Announced Stripe connect account ID(s) for site_url {request.host_url},\
+          to the INTERNAL_STRIPE_CONNECT_ACCOUNT_ANNOUNCE_SERVER: \
+          {current_app.config['INTERNAL_STRIPE_CONNECT_ACCOUNT_ANNOUNCE_SERVER']}",
+        200,
+    )
 
 
 @admin.route("/upload-files", methods=["GET", "POST"])
