78e9b087fdb8164565f8492ff0ed4c49502985e5
chrisjsimpson <chris@karmacomputing.co.uk>
2021-01-03 21:43:28 +0000

    debug playwright #215

--- tests/browser-automated-tests-playwright/index.js
+++ tests/browser-automated-tests-playwright/index.js
@@ -240,10 +240,10 @@ async function test_connect_to_stripe_connect()  {
   await test_order_plan_with_subscription_and_upfront_charge(browsers, browserContextOptions);
   await clearDB();
 
-  await test_order_plan_with_only_upfront_charge(browsers, browserContextOptions);
-  await clearDB();
+//  await test_order_plan_with_only_upfront_charge(browsers, browserContextOptions);
+//  await clearDB();
 
-  //await test_order_plan_with_only_recurring_charge(browsers, browserContextOptions);
-  //await clearDB();
+  await test_order_plan_with_only_recurring_charge(browsers, browserContextOptions);
+  await clearDB();
 
 })();
