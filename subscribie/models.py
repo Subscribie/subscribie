f9b635ebaab3ca8d75df783f58e8346fec53d489
chrisjsimpson <chris@karmacomputing.co.uk>
2021-02-06 15:52:13 +0000

    Fix #425 show num active subscribers on dashboard

--- subscribie/models.py
+++ subscribie/models.py
@@ -8,9 +8,11 @@ import stripe
 from subscribie.utils import (
     get_stripe_secret_key,
     get_stripe_connect_account_id,
+    get_stripe_connect_account,
 )
 
 from .database import database
+import logging
 
 
 def uuid_string():
@@ -88,6 +90,25 @@ class Subscription(database.Model):
     subscribie_checkout_session_id = database.Column(database.String())
     stripe_subscription_id = database.Column(database.String())
     stripe_external_id = database.Column(database.String())
+
+    def stripe_subscription_active(self):
+        if self.stripe_subscription_id is not None:
+
+            stripe.api_key = get_stripe_secret_key()
+            connect_account = get_stripe_connect_account()
+            try:
+                subscription = stripe.Subscription.retrieve(
+                    stripe_account=connect_account.id, id=self.stripe_subscription_id
+                )
+                if subscription.pause_collection is not None:
+                    return False
+                elif subscription.status == "active":
+                    return True
+            except stripe.error.InvalidRequestError as e:
+                logging.error("Could not get stripe subscription status")
+                logging.error(e)
+
+        return False
 
     def upcoming_invoice(self):
         """Return the upcoming invoice (if exists) associated with this Subscription"""
