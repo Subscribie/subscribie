b14ec13adf90f2c864172d864440d8358d341a3c
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-04-19 18:44:01 +0100

    Fix #505 shop owner can create charges against existing subscribers (#511)
    * Fix #505 shop owner can create charges against existing subscribers, with a reference
    
    * add link to transactions on charge subscriber page
    
    * add example for charging amounts ref #505
    
    * padding on view transaction charge subscriber page

--- subscribie/blueprints/admin/subscriber.py
+++ subscribie/blueprints/admin/subscriber.py
@@ -9,3 +9,10 @@ from flask import render_template
 def show_subscriber(subscriber_id):
     person = Person.query.get(subscriber_id)
     return render_template("admin/subscriber/show_subscriber.html", person=person)
+
+
+@admin.route("/charge/subscriber/<person_id>", methods=["GET"])
+@login_required
+def charge_subscriber(person_id):
+    person = Person.query.get(person_id)
+    return render_template("admin/subscriber/charge_subscriber.html", person=person)
