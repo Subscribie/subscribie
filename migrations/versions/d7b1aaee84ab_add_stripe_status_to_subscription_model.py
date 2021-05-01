c98b2dab1aa4f7a9ee8beb4a4c9d58726cbd14c5
chrisjsimpson <chris@karmacomputing.co.uk>
2021-05-02 00:03:57 +0100

    wip #526 add stripe_status to subscription model
    update_stripe_subscription_statuses subscripton.py

---
+++ migrations/versions/d7b1aaee84ab_add_stripe_status_to_subscription_model.py
@@ -0,0 +1,25 @@
+"""add stripe_status to subscription model
+
+Revision ID: d7b1aaee84ab
+Revises: c751fe53a042
+Create Date: 2021-05-01 23:05:25.344813
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "d7b1aaee84ab"
+down_revision = "c751fe53a042"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("subscription") as batch_op:
+        batch_op.add_column(sa.Column("stripe_status", sa.String(), nullable=True))
+
+
+def downgrade():
+    pass
---
+++ subscribie/blueprints/admin/subscription.py
@@ -0,0 +1,37 @@
+from subscribie.models import Subscription
+from subscribie.database import database
+from subscribie.utils import (
+    get_stripe_secret_key,
+    get_stripe_connect_account,
+    stripe_connect_active,
+)
+import stripe
+import logging
+
+
+def update_stripe_subscription_statuses():
+    """Update Stripe subscriptions with their current status
+    by querying Stripe api"""
+    stripe.api_key = get_stripe_secret_key()
+    connect_account = get_stripe_connect_account()
+    if stripe.api_key is None:
+        logging.error("Stripe api key not set refusing to update subscription statuses")
+    if connect_account is None:
+        logging.error(
+            "Stripe connect account not set. Refusing to update subscription statuses"
+        )
+    if stripe_connect_active():
+        for subscription in Subscription.query.all():
+            try:
+                stripeSubscription = stripe.Subscription.retrieve(
+                    stripe_account=connect_account.id,
+                    id=subscription.stripe_subscription_id,
+                )
+                subscription.stripe_status = stripeSubscription.status
+                database.session.commit()
+            except Exception as e:
+                logging.warning(f"Could not update stripe subscription status: {e}")
+    else:
+        logging(
+            "Refusing to update subscription status since Stripe connect is not active"
+        )
