5f93d7141b5f9de013011c3ec8ce83a5c9d4d7e6
Joel <joel@karmacomputing.co.uk>
2021-06-07 17:57:13 -0400

    updating renameshop.sh & validating only letters & numbers input

--- rename-shop.sh
+++ rename-shop.sh
@@ -3,40 +3,39 @@ set -x # Prints each command before executing it
 set -e # Exit the script immidietly if an error happens
 
 # Usage:
-# ./rename-shop.sh <old-name> <new-name>
+# ./rename-shop.sh <old-domain> <new-name>
 #
 
-
-OLD_NAME=$1
+OLD_DOMAIN=$1
 NEW_NAME=$2
-DOMAIN="subscriby.shop"
-INI_FILE="subscriby.shop.ini"
+NEW_DOMAIN=$NEW_NAME".subscriby.shop"
+NEW_INI=$NEW_DOMAIN".ini"
 
-echo Old name: $OLD_NAME
-echo New name: $NEW_NAME
+echo Old name: $OLD_DOMAIN
+echo New name: $NEW_DOMAIN
 
 # Check that the new-name does not already exist
-if [ -d $NEW_NAME.$DOMAIN ]
+if [ -d $NEW_DOMAIN ]
 then
-  echo "$NEW_NAME.subscriby.shop already exists"
+  echo "$NEW_DOMAIN already exists"
   exit 1
 fi
 # this will only be uselfull for admins if not is useless OLD name will be requested
-#if [ ! -d $OLD_NAME.$DOMAIN  ]
+#if [ ! -d $OLD_DOMAIN  ]
 #then
-#  echo "$OLD_NAME may be misspelled"
+#  echo "$OLD_DOMAIN may be misspelled"
 #  exit 1
 #fi
 
 # Rename the directory from old-name to new-name
-mv $OLD_NAME  $NEW_NAME.$DOMAIN #rename directory
-mv $NEW_NAME.$DOMAIN/$OLD_NAME.ini $NEW_NAME.$DOMAIN/$NEW_NAME.$INI_FILE #rename ini file
+mv $OLD_DOMAIN  $NEW_DOMAIN #rename directory
+mv $NEW_DOMAIN/$OLD_DOMAIN.ini $NEW_DOMAIN/$NEW_INI #rename ini file
 
 # Update the .ini file
-sed -i.bk "s/$OLD_NAME/$NEW_NAME/g" $NEW_NAME.$DOMAIN/$NEW_NAME.$INI_FILE 
+sed -i.bk "s/$OLD_DOMAIN/$NEW_DOMAIN/g" $NEW_DOMAIN/$NEW_INI
 # Update the .env file
-sed -i.bk "s/$OLD_NAME/$NEW_NAME/g" $NEW_NAME.$DOMAIN/.env
+sed -i.bk "s/$OLD_DOMAIN/$NEW_DOMAIN/g" $NEW_DOMAIN/.env
 # Rename the .ini file, without this, the uwsigi server keeps the old config
-mv $NEW_NAME.$DOMAIN/$NEW_NAME.$INI_FILE $NEW_NAME.$DOMAIN/$NEW_NAME.disabled
-mv $NEW_NAME.$DOMAIN/$NEW_NAME.disabled $NEW_NAME.$DOMAIN/$NEW_NAME.$INI_FILE
+mv $NEW_DOMAIN/$NEW_INI $NEW_DOMAIN/$NEW_NAME.disabled
+mv $NEW_DOMAIN/$NEW_NAME.disabled $NEW_DOMAIN/$NEW_INI
 
