094174441e975a2eacd108e952632f2c0e875311
Joel <joel@karmacomputing.co.uk>
2021-06-22 19:09:52 -0400

    changing adding-env-setting.sh echo messages

--- adding-env-settings.sh
+++ adding-env-settings.sh
@@ -4,7 +4,7 @@
 #2. Merge the pull request (all sites will reload automatically)
 
 PATH_TO_SITES=$2
-KEYWORD=$1
+NEW_SETTINGS=$1
 if [ "$#" -ne 2 ]
   then
       echo "adding-settings-env.sh missing arguments"
@@ -14,14 +14,15 @@ if [ "$#" -ne 2 ]
       echo "EXAMPLE"
       echo "./adding-settings-env.sh PATH_TO_SITES='/etc/home/' /etc/"
 else      
-    find $PATH_TO_SITES -maxdepth 2 -mindepth 2 -type f -name '.env' -exec  grep $KEYWORD {} +
+    find $PATH_TO_SITES -maxdepth 2 -mindepth 2 -type f -name '.env' -exec  grep $NEW_SETTINGS {} +
     if [ $? -ne 0 ] 
       then 
-         echo "Doesnt Exist, adding variable"
-         find $PATH_TO_SITES -maxdepth 2 -mindepth 2 -type f -name '.env' -exec  sh -c "echo $KEYWORD >> {}" \;
+         echo "Adding new env setting..."
+         find $PATH_TO_SITES -maxdepth 2 -mindepth 2 -type f -name '.env' -exec  sh -c "echo $NEW_SETTINGS >> {}" \;
+         echo "env setting succesfully added"
          exit 0
     else
-        echo "that setting already exists"
+        echo "The setting already exists"
         exit 1
     fi
 fi
