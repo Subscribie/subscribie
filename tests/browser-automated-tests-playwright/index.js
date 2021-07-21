cabe81df44bec80d87edcb7fa28da0a35e775ed5
joeltejeda <76879536+joeltejeda@users.noreply.github.com>
2021-07-21 18:20:15 -0400

    652 refund feature fix and adding playwright (#653)
    * changing header
    
    * removing reverse transversal
    
    * adding automated refund test
    
    * #652 add note test_transaction_refund requires a non refunded transaction to be created prior to this test
    
    Co-authored-by: chrisjsimpson <chris.j.simpson@live.co.uk>

--- tests/browser-automated-tests-playwright/index.js
+++ tests/browser-automated-tests-playwright/index.js
@@ -4,6 +4,7 @@ test_order_plan_with_subscription_and_upfront_charge = require('./tests/test_ord
 test_order_plan_with_only_upfront_charge = require('./tests/test_order_plan_with_only_upfront_charge');
 test_order_plan_with_only_recurring_charge = require('./tests/test_order_plan_with_only_recurring_charge');
 test_transaction_filter_by_name_and_by_plan_title = require('./tests/test_transaction_filter_by_name_and_by_plan_title');
+test_transaction_refund = require('./tests/test_transaction_refund');
 test_create_free_trial_plan = require('./tests/test_create_free_trial_plan');
 const playwright = require('playwright');
 const fs = require('fs');
@@ -285,12 +286,16 @@ async function test_connect_to_stripe_connect()  {
   await clearDB();
   await test_order_plan_with_subscription_and_upfront_charge(browsers, browserContextOptions);
   await test_transaction_filter_by_name_and_by_plan_title(browsers, browserContextOptions);
-  
+  //Note: test_transaction_refund requires a non refunded transaction to be created prior to
+  // this test.
+  await test_transaction_refund(browsers, browserContextOptions);
+
   await clearDB();
   await test_create_free_trial_plan(browsers, browserContextOptions);
   
   await clearDB();
   await test_order_plan_with_only_upfront_charge(browsers, browserContextOptions);
+
   await clearDB();
 
 })();
