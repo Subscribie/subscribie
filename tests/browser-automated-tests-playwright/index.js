34db1f0c8d7bd97cccd9f024d9ba8c4c495c39f7
Joel <joel@karmacomputing.co.uk>
2021-07-20 20:52:30 -0400

    #657 adding free trial test feature

--- tests/browser-automated-tests-playwright/index.js
+++ tests/browser-automated-tests-playwright/index.js
@@ -4,6 +4,7 @@ test_order_plan_with_subscription_and_upfront_charge = require('./tests/test_ord
 test_order_plan_with_only_upfront_charge = require('./tests/test_order_plan_with_only_upfront_charge');
 test_order_plan_with_only_recurring_charge = require('./tests/test_order_plan_with_only_recurring_charge');
 test_transaction_filter_by_name_and_by_plan_title = require('./tests/test_transaction_filter_by_name_and_by_plan_title');
+test_add_free_trial_plan = require('./tests/test_add_free_trial_plan');
 const playwright = require('playwright');
 const fs = require('fs');
 const { devices } = require('playwright');
@@ -284,6 +285,9 @@ async function test_connect_to_stripe_connect()  {
   await clearDB();
   await test_order_plan_with_subscription_and_upfront_charge(browsers, browserContextOptions);
   await test_transaction_filter_by_name_and_by_plan_title(browsers, browserContextOptions);
+  
+  await clearDB();
+  await test_add_free_trial_plan(browsers, browserContextOptions);
   
   await clearDB();
   await test_order_plan_with_only_upfront_charge(browsers, browserContextOptions);
