2bd603be1dcad14f2fd3acc1abfbf752f60db89b
Joel <joel@karmacomputing.co.uk>
2021-08-09 18:42:32 -0400

    changing screenshot naming

--- tests/browser-automated-tests-playwright/tests/order_plan_with_recurring_and_upfront_charge.js
+++ tests/browser-automated-tests-playwright/tests/order_plan_with_recurring_and_upfront_charge.js
@@ -32,12 +32,12 @@ test.describe("order plan with recurring and upfront charge test:", () => {
         await page.fill('#address_line_one', '123 Short Road');
         await page.fill('#city', 'London');
         await page.fill('#postcode', 'L01 T3U');
-        expect(await page.screenshot()).toMatchSnapshot('new-customer-form.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-new-customer-form.png');
         await page.click('.btn-primary-lg');
         // Begin stripe checkout
         const order_summary_content = await page.textContent(".title-1");
         expect(order_summary_content === "Order Summary");
-        expect(await page.screenshot()).toMatchSnapshot('pre-stripe-checkout.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-pre-stripe-checkout.png');
         await page.click('#checkout-button');
 
         //Verify first payment is correct (upfront charge + first recuring charge)
@@ -54,26 +54,26 @@ test.describe("order plan with recurring and upfront charge test:", () => {
         await page.selectOption('select#billingCountry', 'GB');
 
         await page.fill('#billingPostalCode', 'LN1 7FH');
-        expect(await page.screenshot()).toMatchSnapshot('stripe-checkout-filled.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-stripe-checkout-filled.png');
         await page.click('.SubmitButton');
     
         // Verify get to the thank you page order complete
         const order_complete_content = await page.textContent('.title-1');
         expect(order_complete_content === "Order Complete!");
-        expect(await page.screenshot()).toMatchSnapshot('order-complete.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-order-complete.png');
 
         // Go to My Subscribers page
         // Crude wait before we check subscribers to allow webhooks time
         await new Promise(x => setTimeout(x, 5000)); //5 secconds
         await page.goto('admin/subscribers')
-        expect(await page.screenshot()).toMatchSnapshot('view-subscribers.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-view-subscribers.png');
         
         // Click Refresh Subscription
         await page.click('#refresh_subscriptions'); // this is the refresh subscription
         await page.textContent('.alert-heading') === "Notification";
         // screeshot to the active subscriber
         await page.goto('admin/dashboard');
-        expect(await page.screenshot()).toMatchSnapshot('active-subscribers.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-active-subscribers.png');
         //go back to subscriptions
         await page.goto('admin/subscribers')
         
@@ -97,7 +97,7 @@ test.describe("order plan with recurring and upfront charge test:", () => {
 
         // Verify paid invoice has been generated & marked "paid":
         await page.goto('admin/invoices')
-        expect(await page.screenshot()).toMatchSnapshot('view-paid-invoices.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-view-paid-invoices.png');
         const content_paid_invoice_status = await page.textContent('.invoice-status');
         expect(content_paid_invoice_status === 'paid')
 
@@ -109,7 +109,7 @@ test.describe("order plan with recurring and upfront charge test:", () => {
         // Fetch Upcoming Invoices
         await page.click('#fetch_upcoming_invoices');
         await new Promise(x => setTimeout(x, 10000)); //10 secconds
-        expect(await page.screenshot()).toMatchSnapshot('view-upcoming-invoices.png');
+        expect(await page.screenshot()).toMatchSnapshot('recurring-and-upfront-view-upcoming-invoices.png');
         const content_upcoming_invoice_amount = await page.textContent('.upcoming-invoice-amount');
         expect(content_upcoming_invoice_amount === '£5.99')
         
