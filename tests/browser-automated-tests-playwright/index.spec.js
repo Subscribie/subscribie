e45eb0ab86670e48bf19175c268593b40451065c
Joel <joel@karmacomputing.co.uk>
2021-08-25 20:30:48 -0400

    announcer logs wording

--- tests/browser-automated-tests-playwright/index.spec.js
+++ tests/browser-automated-tests-playwright/index.spec.js
@@ -165,7 +165,7 @@ test.describe("Subscribie tests:", () => {
       console.log("Stripe Connected");
       await page.goto('/admin/announce-stripe-connect'); 
       await page.textContent(':has-text("Announced Stripe connect account")') === "Announced Stripe connect account";
-      console.log("Announced to Stripe");
+      console.log("Announced to Stripe connect account");
 
   }); 
   plan_creation = require('./tests/plan_creation');
