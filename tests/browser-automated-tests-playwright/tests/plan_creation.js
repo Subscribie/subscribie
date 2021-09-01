2c46fab75bc6178b6983f8a79992419b896c2bf9
Joel <joel@karmacomputing.co.uk>
2021-09-01 16:59:07 -0400

    creating plan with choice, options and required buyer note

--- tests/browser-automated-tests-playwright/tests/plan_creation.js
+++ tests/browser-automated-tests-playwright/tests/plan_creation.js
@@ -183,6 +183,126 @@ test.describe("Plan Creation tests:", () => {
      }
   });
 
+  test.only("Create plan with options, choice, required description", async ({ page }) => {
+    await page.goto('/');
+    try {
+      page.setDefaultTimeout(3000);
+      const check_plan_with_choice_and_options = await page.textContent('text="Plan with choice and options"');
+      expect(check_plan_with_choice_and_options === "Plan with choice and options");
+      await page.click("text=See choice options");
+      await page.click("text=Choices (2 options)");
+      //check if plan options are blue and red
+      const check_plan_option_red = await page.textContent('text="Red"');
+      expect(check_plan_option_red === "Red");
+      const check_plan_option_blue = await page.textContent('text="Blue"');
+      expect(check_plan_option_blue === "Blue");
+
+      const expand_choice = await page.textContent('text="Choices (2 options)"');
+      if (expand_choice === "Choices (2 options)") {
+ 
+        await new Promise(x => setTimeout(x, 1000));
+        expect(await page.screenshot()).toMatchSnapshot('plan-with-choice-and-options-already-created.png');
+        console.log("Plan with choice and options already created, exiting test");
+        return 0;
+      }
+    } catch (e) {
+      console.log("Continuing with Plan with choice and options creation");
+    }
+    // Go to add plan
+    await page.goto('/admin/add');
+    
+    //Fill plan 
+    await page.fill('#title-0', 'Plan with choice and options');
+    await page.fill('#selling_points-0-0', 'Plan with ');
+    await page.fill('#selling_points-0-1', 'Choice and options');
+    await page.fill('#selling_points-0-3', 'with required description');
+
+    //wait for the recurring charge to expand
+    await page.click('.form-check-input');
+    const monthly_content = await page.textContent('#interval_amount_label');
+    expect(monthly_content === "Recurring Amount");
+    await page.fill('#interval_amount-0', '15');
+
+    //fill plan description 
+    await page.click('#plan_description_required-0');
+    await page.fill('#description-0','This plan requires a user description, options and choice selection');
+    
+    //Require customer note
+    await page.click('#note_to_seller_required-0');
+    await page.fill('#note_to_buyer_message-0','Please add another colour');
+
+    await page.click('text="Save"');
+    //Check if plan was created 
+    await page.goto('/');
+    const plan_with_choice_and_options = await page.textContent('text="Plan with choice and options"');
+    expect(plan_with_choice_and_options === "Plan with choice and options");
+    //screenshot
+    await new Promise(x => setTimeout(x, 1000));
+    expect(await page.screenshot()).toMatchSnapshot('plan-with-choice-and-options-created.png');
+    
+    //Add options and choices
+    console.log('adding plan options and choices');
+    await page.goto('/admin/add-choice-group');
+
+    //add choice group
+    const add_choice_group = await page.textContent('text="Add Choice Group"');
+    expect(add_choice_group === "Add Choice Group");
+    await page.fill('.form-control','Choices');
+    await page.click("text='Save'");
+    await page.textContent('.alert-heading') === "Notification";
+    console.log("Choice Created");
+
+    //add first option
+    console.log("adding options...")
+    await page.goto('/admin/list-choice-groups');
+    await page.click("text=Options");
+
+    const add_options = await page.textContent('text="Option"');
+    expect(add_options === "Option");
+    await page.click("text=Add Option");
+    await page.fill('.form-control','Red');
+    await page.click("text='Save'");
+    await page.textContent('.alert-heading') === "Notification";
+    console.log("First Option added");
+
+    //add second option
+    await page.click("text=Add Option");
+    await page.fill('.form-control','Blue');
+    await page.click("text='Save'");
+    await page.textContent('.alert-heading') === "Notification";
+    console.log("Second Option added");
+
+    //assign choice to plan
+    console.log("Assigning Choice to plan...")
+    await page.goto('/admin/list-choice-groups');
+    await page.click("text=Assign Plan");
+    const assign_choice_to_plan = await page.textContent('text="Choice Group - Assign Plan"');
+    expect(assign_choice_to_plan === "Choice Group - Assign Plan");
+    await page.click("text=Plan with choice and options");
+    await page.click("text='Save'");
+    await page.textContent('.alert-heading') === "Notification";
+    console.log("Choice assigned to plan");
+
+    //confirm choice and option plan was added
+    await page.goto('/');
+    const free_trial = await page.textContent('text="Plan with choice and options"');
+    expect(free_trial === "Plan with choice and options");
+
+    //check if plan options are blue and red
+    const check_plan_option_red = await page.textContent('text="Red"');
+    expect(check_plan_option_red === "Red");
+    const check_plan_option_blue = await page.textContent('text="Blue"');
+    expect(check_plan_option_blue === "Blue");
+
+    //check if plan have choice
+    await page.click("text=See choice options");
+    await page.click("text=Choices (2 options)");
+    const expand_choice = await page.textContent('text="Choices (2 options)"');
+    expect(expand_choice === "Choices (2 options)");
+    expect(await page.screenshot()).toMatchSnapshot('plan-with-choice-and-options-created.png');
+    console.log("Plan with option and choices shown in homepage");
+  });
+
 });
 
 //module.exports = plan_creation;
