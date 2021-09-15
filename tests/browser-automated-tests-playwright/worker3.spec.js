50a96dca0876efc1e3a8784c8fdc67c2bd469733
Joel <joel@karmacomputing.co.uk>
2021-09-15 11:14:50 -0400

    plawright test plan categories, public page and slogan

--- tests/browser-automated-tests-playwright/worker3.spec.js
+++ tests/browser-automated-tests-playwright/worker3.spec.js
@@ -1,5 +1,3 @@
-
-
 const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
@@ -14,4 +12,10 @@ test.beforeEach(async ({ page }) => {
   }); 
   clear_DB = require('./tests/clear_db');
 
+  categories_creation = require('./tests/categories_creation');
+
   private_page_creation = require('./tests/private_page_creation');
+
+  public_page_creation = require('./tests/public_page_creation');
+
+  slogan_creation = require('./tests/slogan_creation');
