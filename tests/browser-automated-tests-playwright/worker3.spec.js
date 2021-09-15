2c52c8b0dd1e737b78fd594a820f54bcf84dda4b
Joel <joel@karmacomputing.co.uk>
2021-09-15 17:39:02 -0400

    playwright test change style shop, charge and show invoices with VAT

--- tests/browser-automated-tests-playwright/worker3.spec.js
+++ tests/browser-automated-tests-playwright/worker3.spec.js
@@ -19,3 +19,9 @@ test.beforeEach(async ({ page }) => {
   public_page_creation = require('./tests/public_page_creation');
 
   slogan_creation = require('./tests/slogan_creation');
+
+  change_shop_colour = require('./tests/change_shop_colour');
+
+  adding_vat = require('./tests/adding_vat');
+
+  ordering_plan_with_VAT = require('./tests/ordering_plan_with_VAT');
