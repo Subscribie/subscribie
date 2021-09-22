069109a0eec773c71dbd1cd6d99f90dccd257368
Joel <joel@karmacomputing.co.uk>
2021-09-22 14:08:11 -0400

    adding github action mail sending into a new worker

---
+++ tests/browser-automated-tests-playwright/github.spec.js
@@ -0,0 +1,6 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe("Github actions test:", () => {
+magic_login_receives_email = require('./tests/704_shop_owner_magic_login_receives_email');
+
+});
