71c09acb97e5a5d181c7aa38b016cfaa09e1a799
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-10-15 00:13:57 +0100

    hotfix dont perform archive filter in /new_customer
    todo refactor this, no need with newer version of sqlalchemy

--- subscribie/models.py
+++ subscribie/models.py
@@ -55,6 +55,8 @@ def filter_archived(query):
             and "/admin/transactions" not in request.path
             and "/static" not in request.path
             and "/admin/dashboard" not in request.path
+            and "/page" not in request.path
+            and "/new_customer" not in request.path
         ):
             query = query.filter(entity.archived == 0)
             return query
