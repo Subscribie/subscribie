b096b558f2c31f107c36429c4f481ffaa4583cd1
Joel <joel@karmacomputing.co.uk>
2022-02-24 16:40:05 -0400

    wip changing font colors

--- subscribie/blueprints/style/__init__.py
+++ subscribie/blueprints/style/__init__.py
@@ -15,6 +15,7 @@ def inject_custom_style():
     # output as inline css using <style> tags.
     global_css = ""
     js_inject = ""
+    font_js_inject = ""
     # Set default colours
     styles = ModuleStyle.query.first()
     if styles is not None:
@@ -23,21 +24,36 @@ def inject_custom_style():
     # Js primary colours injection via javascript
     try:
         css_properties_json = json.loads(styles.css_properties_json)
-        primary = css_properties_json["primary"]
-        hsl_h = primary.split(",")[0]
-        hsl_s = primary.split(",")[1]
-        hsl_l = primary.split(",")[2]
-        js_inject = """<script>
-        document.documentElement.style.setProperty('--primary-color-hs', [Math.round({hsl_h}), Math.round({hsl_s} * 100) + '%'].join());
-        document.documentElement.style.setProperty('--primary-color-l', Math.round({hsl_l} * 100) + '%');
-        </script>
-        """.format(
-            hsl_h=hsl_h, hsl_s=hsl_s, hsl_l=hsl_l
-        )
+        if "primary" in css_properties_json:
+            primary = css_properties_json["primary"]
+            hsl_h = primary.split(",")[0]
+            hsl_s = primary.split(",")[1]
+            hsl_l = primary.split(",")[2]
+            js_inject = """<script>
+            document.documentElement.style.setProperty('--primary-color-hs', [Math.round({hsl_h}), Math.round({hsl_s} * 100) + '%'].join());
+            document.documentElement.style.setProperty('--primary-color-l', Math.round({hsl_l} * 100) + '%');
+            </script>
+            """.format(
+                hsl_h=hsl_h, hsl_s=hsl_s, hsl_l=hsl_l
+            )
+        if "font" in css_properties_json:
+            font = css_properties_json["font"]
+            f_hsl_h = font.split(",")[0]
+            f_hsl_s = font.split(",")[1]
+            f_hsl_l = font.split(",")[2]
+            font_js_inject = """<script>
+            document.documentElement.style.setProperty('--font-color-hs', [Math.round({f_hsl_h}), Math.round({f_hsl_s} * 100) + '%'].join());
+            document.documentElement.style.setProperty('--font-color-l', Math.round({f_hsl_l} * 100) + '%');
+            </script>
+            """.format(
+                f_hsl_h=f_hsl_h, f_hsl_s=f_hsl_s, f_hsl_l=f_hsl_l
+            )
     except Exception as e:
         log.warning(f"Could not load custom css properties. {e}")
     # Raw global css overrises
-    custom_css = "".join([js_inject, '<style type="text/css">', global_css, "</style>"])
+    custom_css = "".join(
+        [js_inject, font_js_inject, '<style type="text/css">', global_css, "</style>"]
+    )
 
     return dict(custom_css=custom_css)
 
@@ -65,8 +81,12 @@ def style_shop():
         if css_style is not None and css_style.css_properties_json != "":
             try:
                 css_properties = json.loads(css_style.css_properties_json)
-                parts = css_properties["primary"].split(",")
-                css_primary = f"hsl({parts[0]}, {parts[1]}, {parts[2]})"
+                if "primary" in css_properties:
+                    parts = css_properties["primary"].split(",")
+                    css_primary = f"hsl({parts[0]}, {parts[1]}, {parts[2]})"
+                if "font" in css_properties:
+                    parts = css_properties["font"].split(",")
+                    css_primary = f"hsl({parts[0]}, {parts[1]}, {parts[2]})"
             except Exception as e:
                 log.error(f"Could not load css_style.css_properties_json. {e}")
         else:
@@ -101,5 +121,28 @@ def save_custom_style():
 
     database.session.add(styles)
     database.session.commit()
+    return redirect("/")
 
+
+@module_style_shop.route("/style-shop-font", methods=["POST"])
+@login_required
+def save_custom_font():
+    """Remove all old css, and replace with submitted css"""
+    styles = ModuleStyle.query.first()
+    if styles is None:
+        styles = ModuleStyle()
+
+    if request.form.get("global_css", None):
+        # Save global css
+        global_css = request.form.get("global_css", "")
+        styles.css = global_css
+    else:
+        styles.css = ""  # Remove global css styles as empty
+        # Save custom hsl colours
+        css_properties = json.dumps(request.form.to_dict())
+        if "font" in css_properties:
+            styles.css_properties_json = css_properties
+
+    database.session.add(styles)
+    database.session.commit()
     return redirect("/")
