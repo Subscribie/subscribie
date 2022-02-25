c4c903c3a093191825f67435789c93c105d82c5c
Joel <joel@karmacomputing.co.uk>
2022-02-25 17:57:49 -0400

    wip changing background and color

--- subscribie/blueprints/style/__init__.py
+++ subscribie/blueprints/style/__init__.py
@@ -24,8 +24,8 @@ def inject_custom_style():
     # Js primary colours injection via javascript
     try:
         css_properties_json = json.loads(styles.css_properties_json)
-        if "primary" in css_properties_json:
-            primary = css_properties_json["primary"]
+        if "primary-background-colour" in css_properties_json:
+            primary = css_properties_json["primary-background-colour"]
             hsl_h = primary.split(",")[0]
             hsl_s = primary.split(",")[1]
             hsl_l = primary.split(",")[2]
@@ -36,8 +36,8 @@ def inject_custom_style():
             """.format(
                 hsl_h=hsl_h, hsl_s=hsl_s, hsl_l=hsl_l
             )
-        if "font" in css_properties_json:
-            font = css_properties_json["font"]
+        if "primary-font-colour" in css_properties_json:
+            font = css_properties_json["primary-font-colour"]
             f_hsl_h = font.split(",")[0]
             f_hsl_s = font.split(",")[1]
             f_hsl_l = font.split(",")[2]
@@ -75,18 +75,19 @@ def style_shop():
         # Load custom css rules (if any) and display in an editable textbox
         css_style = ModuleStyle.query.first()
         css_primary = ""
+        css_font = ""
         customCSS = ""
         if css_style is not None and css_style.css != "":
             customCSS = css_style.css
         if css_style is not None and css_style.css_properties_json != "":
             try:
                 css_properties = json.loads(css_style.css_properties_json)
-                if "primary" in css_properties:
-                    parts = css_properties["primary"].split(",")
+                if "primary-background-colour" in css_properties:
+                    parts = css_properties["primary-background-colour"].split(",")
                     css_primary = f"hsl({parts[0]}, {parts[1]}, {parts[2]})"
-                if "font" in css_properties:
-                    parts = css_properties["font"].split(",")
-                    css_primary = f"hsl({parts[0]}, {parts[1]}, {parts[2]})"
+                if "primary-font-colour" in css_properties:
+                    parts = css_properties["primary-font-colour"].split(",")
+                    css_font = f"hsl({parts[0]}, {parts[1]}, {parts[2]})"
             except Exception as e:
                 log.error(f"Could not load css_style.css_properties_json. {e}")
         else:
@@ -94,7 +95,10 @@ def style_shop():
             css_properties = ""
 
         return render_template(
-            "show-custom-css.html", customCSS=customCSS, css_primary=css_primary
+            "show-custom-css.html",
+            customCSS=customCSS,
+            css_primary=css_primary,
+            css_font=css_font,
         )
     except TemplateNotFound:
         abort(404)
@@ -114,34 +118,50 @@ def save_custom_style():
         styles.css = global_css
     else:
         styles.css = ""  # Remove global css styles as empty
-        # Save custom hsl colours
-        css_properties = json.dumps(request.form.to_dict())
-        if "primary" in css_properties:
-            styles.css_properties_json = css_properties
 
     database.session.add(styles)
     database.session.commit()
     return redirect("/")
 
 
-@module_style_shop.route("/style-shop-font", methods=["POST"])
+@module_style_shop.route("/style-shop-background-colour", methods=["POST"])
+@login_required
+def save_custom_background_colour():
+
+    # Save custom hsl colours
+    styles = ModuleStyle.query.first()
+    backgroundCssProperties = {}
+    try:
+        backgroundCssProperties = json.loads(styles.css_properties_json)
+    except AttributeError:
+        log.info("First time getting backgroud color")
+
+        backgroundCssProperties["primary-background-colour"] = request.form.to_dict()[
+            "primary-background-colour"
+        ]
+
+    styles.css_properties_json = json.dumps(backgroundCssProperties)
+
+    database.session.add(styles)
+    database.session.commit()
+    return redirect("/")
+
+
+@module_style_shop.route("/style-shop-font-colour", methods=["POST"])
 @login_required
 def save_custom_font():
     """Remove all old css, and replace with submitted css"""
     styles = ModuleStyle.query.first()
-    if styles is None:
-        styles = ModuleStyle()
+    try:
+        json.loads(styles.css_properties_json)
+    except AttributeError:
+        fontCssProperties = {}
+        log.info("First time getting font colour")
 
-    if request.form.get("global_css", None):
-        # Save global css
-        global_css = request.form.get("global_css", "")
-        styles.css = global_css
-    else:
-        styles.css = ""  # Remove global css styles as empty
-        # Save custom hsl colours
-        css_properties = json.dumps(request.form.to_dict())
-        if "font" in css_properties:
-            styles.css_properties_json = css_properties
+        fontCssProperties["primary-font-colour"] = request.form.to_dict()[
+            "primary-font-colour"
+        ]
+    styles.css_properties_json = json.dumps(fontCssProperties)
 
     database.session.add(styles)
     database.session.commit()
