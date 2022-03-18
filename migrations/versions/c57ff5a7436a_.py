b646a8602f7eec51d56ee5cde54431bfc652182d
Joel <joel@karmacomputing.co.uk>
2022-03-18 19:50:57 -0400

    merging alembic revision stripe_invoice and balance table

---
+++ migrations/versions/c57ff5a7436a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c57ff5a7436a
+Revises: 5e756a48f86d, 7fba2877d034
+Create Date: 2022-03-15 19:30:28.091766
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c57ff5a7436a'
+down_revision = ('5e756a48f86d', '7fba2877d034')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
