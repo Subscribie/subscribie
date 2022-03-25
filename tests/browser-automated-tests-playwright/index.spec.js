e43041f6a76473156056ad41ebd72993a91ce762
Joel <joel@karmacomputing.co.uk>
2022-03-25 03:18:45 -0400

    #831 reducing naming ambiguity

--- tests/browser-automated-tests-playwright/index.spec.js
+++ tests/browser-automated-tests-playwright/index.spec.js
@@ -185,6 +185,6 @@ test.describe("Subscribie tests:", () => {
   // 2. 2.A pause, resume and 2.B cancel subscription test. 
   order_plan_with_subscription_and_upfront_charge = require('./tests/293_subscriber_order_plan_with_recurring_and_upfront_charge');
 
-  clearing_connect_account_id = require('./tests/clear_connect_account_id.js');
+  delete_connect_account_id = require('./tests/delete_connect_account_id.js');
 });
 
