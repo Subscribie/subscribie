2ba3945bae9cd51ef102169e1dea2d9f266e20ae
Joel <joel@karmacomputing.co.uk>
2022-03-25 03:10:34 -0400

    adding connect-account-id deletion after the test ends

---
+++ tests/browser-automated-tests-playwright/tests/clear_connect_account_id.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@playwright/test');
+// Clear connect account id after all tests are completed
+test("@831@Clearing-connect-account-id", async ({ page }) => {
+
+    await page.goto('/admin/delete-connect-account');
+    await new Promise(x => setTimeout(x, 2000)); //2 secconds
+    await page.goto('admin/dashboard')
+    const connect_to_stripe = await page.textContent('text="Review Stripe"');
+    expect (connect_to_stripe == 'Review Stripe');
+    
+
+}); 
