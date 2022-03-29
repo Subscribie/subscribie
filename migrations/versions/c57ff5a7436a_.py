c6207fff899dedc2bd83fe564a6d9405a1a3c05b
Joel <joel@karmacomputing.co.uk>
2022-03-29 18:13:10 -0400

    deleting unused imports

--- migrations/versions/c57ff5a7436a_.py
+++ migrations/versions/c57ff5a7436a_.py
@@ -5,13 +5,10 @@ Revises: 5e756a48f86d, 7fba2877d034
 Create Date: 2022-03-15 19:30:28.091766
 
 """
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
-revision = 'c57ff5a7436a'
-down_revision = ('5e756a48f86d', '7fba2877d034')
+revision = "c57ff5a7436a"
+down_revision = ("5e756a48f86d", "7fba2877d034")
 branch_labels = None
 depends_on = None
 
