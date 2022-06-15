125347630aae3d803756d20642c224bfa1f74164
Joel <joel@karmacomputing.co.uk>
2022-06-15 18:56:52 -0400

    refactoring email sending to magic login and reset password

--- tests/browser-automated-tests-playwright/tests/704_shop_owner_magic_login_receives_email.js
+++ tests/browser-automated-tests-playwright/tests/704_shop_owner_magic_login_receives_email.js
@@ -1,12 +1,12 @@
 const { test, expect } = require('@playwright/test');
 const checkShopOwnerLogin = require('./checkShopOwnerLogin.js');
 
-const TEST_SHOP_OWNER_LOGIN_URL = process.env.TEST_SHOP_OWNER_LOGIN_URL_ISSUE_704
-const TEST_SHOP_OWNER_EMAIL = process.env.TEST_SHOP_OWNER_EMAIL_ISSUE_704
+const SHOP_OWNER_LOGIN_URL = process.env.SHOP_OWNER_LOGIN_URL
+const SHOP_OWNER_EMAIL = process.env.SHOP_OWNER_EMAIL_USER
 
 test('@704@shop_owner@magic login receives email', async ({ page }) => {
-  await page.goto(TEST_SHOP_OWNER_LOGIN_URL);
-  await page.fill('#email', TEST_SHOP_OWNER_EMAIL);
+  await page.goto(SHOP_OWNER_LOGIN_URL);
+  await page.fill('#email', SHOP_OWNER_EMAIL);
   await page.click('#login');
   await new Promise(r => setTimeout(r, 5000));
   checkShopOwnerLogin.checkShopOwnerLogin();
