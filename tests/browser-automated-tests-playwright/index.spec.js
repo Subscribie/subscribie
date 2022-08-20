7b033524b8f84a6ba3fe61e30e9488d43cf748d0
Joel <joel@karmacomputing.co.uk>
2022-08-20 17:11:45 -0400

    fixing stripe onboarding test

--- tests/browser-automated-tests-playwright/index.spec.js
+++ tests/browser-automated-tests-playwright/index.spec.js
@@ -19,7 +19,7 @@ test.describe("Subscribie tests:", () => {
     // Check onboarding not already completed
     try {
       let connectYourShopContent = await page.evaluate(() => document.body.textContent);
-      if (connectYourShopContent.indexOf("Congrats!") > -1) {
+      if (connectYourShopContent.indexOf("Your currently running in test mode.") > -1) {
         expect(await page.screenshot()).toMatchSnapshot('connect_stripe-to-shop-dashboard-chromium.png');
         console.log("Already connected Stripe sucessfully, exiting test");
         return 0;
@@ -35,7 +35,7 @@ test.describe("Subscribie tests:", () => {
       await page.goto('admin/connect/stripe-connect');
       //page.setDefaultTimeout(3000);
       let contentStripeConnect = await page.evaluate(() => document.body.textContent);
-      test.skip(contentStripeConnect.indexOf("Congrats!") > -1);
+      test.skip(contentStripeConnect.indexOf("Your currently running in test mode.") > -1);
       expect(await page.screenshot()).toMatchSnapshot('stripe_status.png');
 
       // deleting connect account id, if stripe was not succesfully connected
@@ -169,8 +169,8 @@ test.describe("Subscribie tests:", () => {
 
       console.log("Announce stripe account automatically visiting announce url. In prod this is called via uwsgi cron");
       await new Promise(x => setTimeout(x, 5000));
-      const stripe_connected = await page.textContent("text=Congrats!");
-      expect(stripe_connected === "Congrats!");
+      const stripe_connected = await page.textContent("text=Your currently running in test mode.");
+      expect(stripe_connected === "Your currently running in test mode.");
       console.log("Stripe Connected");
       await page.goto('/admin/announce-stripe-connect'); 
       await page.textContent(':has-text("Announced Stripe connect account")') === "Announced Stripe connect account";
