fce0ac51439a1b9c189ea8c7c1bf5653b8bffb7f
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-10-09 01:03:50 +0100

    #951 bring migrations up to black format

--- migrations/versions/dde6bed9a56f_add_shop_activated_to_setting_model.py
+++ migrations/versions/dde6bed9a56f_add_shop_activated_to_setting_model.py
@@ -10,15 +10,15 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'dde6bed9a56f'
-down_revision = '96430096c2c7'
+revision = "dde6bed9a56f"
+down_revision = "96430096c2c7"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     with op.batch_alter_table("setting") as batch_op:
-        batch_op.add_column(sa.Column('shop_activated', sa.Boolean(), default=0))
+        batch_op.add_column(sa.Column("shop_activated", sa.Boolean(), default=0))
 
 
 def downgrade():
