1554dd29485b28dd65bcd14301f6a0a5ba00cb24
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-10-13 22:22:51 +0100

    wip fix #962 log get_geo_currency_code currency_code

--- subscribie/utils.py
+++ subscribie/utils.py
@@ -43,6 +43,7 @@ def get_geo_currency_code():
     except KeyError as e:
         log.error(f"Could not map country_code {country_code} to a currency code. {e}")
         currency_code = get_shop_default_currency_code()
+    log.debug(f("get_geo_currency_code returned currency_code: {currency_code}"))
     return currency_code
 
 
