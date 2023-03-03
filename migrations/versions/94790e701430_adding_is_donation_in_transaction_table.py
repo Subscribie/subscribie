0dbfb74c25692936dfc23e1e7ba315e5ad9b1ff5
Joel <joel@karmacomputing.co.uk>
2023-03-02 23:14:02 -0400

    adding is_donation column inside transaction table

---
+++ migrations/versions/94790e701430_adding_is_donation_in_transaction_table.py
@@ -0,0 +1,27 @@
+"""adding_is_donation_in_transaction_table
+
+Revision ID: 94790e701430
+Revises: bd63fd27d653
+Create Date: 2023-03-02 19:45:02.205558
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "94790e701430"
+down_revision = "bd63fd27d653"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+
+    with op.batch_alter_table("transactions", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("is_donation", sa.Boolean(), nullable=True))
+
+
+def downgrade():
+
+    pass
