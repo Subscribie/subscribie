29a877e532d394d136beb1085ce7d767db29a1ea
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-05-11 23:56:12 +0100

    Fix #1122 when showing terms-and-conditions-agreed also filter by read_only

--- subscribie/blueprints/admin/__init__.py
+++ subscribie/blueprints/admin/__init__.py
@@ -769,6 +769,7 @@ def list_documents():
         show_only_agreed_documents = True
         documents = (
             Document.query.where(Document.type == "terms-and-conditions-agreed")
+            .execution_options(include_archived=True)
             .where(Document.read_only == True)
             .all()
         )
