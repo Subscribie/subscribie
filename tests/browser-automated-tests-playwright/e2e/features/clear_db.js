6dad6ee74a21acd459c3521173ca993faafcd820
Joel <joel@karmacomputing.co.uk>
2023-06-07 00:22:20 -0400

    updating tests dependencies and adding clead_db tests

--- tests/browser-automated-tests-playwright/e2e/features/clear_db.js
+++ tests/browser-automated-tests-playwright/e2e/features/clear_db.js
@@ -1,17 +1,20 @@
+const { admin_login } = require('./admin_login');
+const { expect } = require('@playwright/test');
+const  URL = process.env['PLAYWRIGHT_HOST'];
 var clear_db = async function (page) {
-    await page.goto('/admin/remove-subscriptions');
+    await page.goto(URL + '/admin/remove-subscriptions');
     const contentSubscriptions = await page.evaluate(() => document.body.textContent.indexOf("all subscriptions deleted"));
     expect(contentSubscriptions > -1);
 
-    await page.goto('/admin/remove-people');
+    await page.goto(URL + '/admin/remove-people');
     const contentPeople = await page.evaluate(() => document.body.textContent.indexOf("all people deleted"));
     expect(contentPeople > -1);
 
-    await page.goto('/admin/remove-transactions');
+    await page.goto(URL + '/admin/remove-transactions');
     const contentTransactions = await page.evaluate(() => document.body.textContent.indexOf("all transactions deleted"));
     expect(contentTransactions > -1);
 
-    await page.goto('/admin/remove-documents');
+    await page.goto(URL + '/admin/remove-documents');
     const contentDocuments = await page.evaluate(() => document.body.textContent.indexOf("all documents deleted"));
     expect(contentDocuments > -1);
     // End Clear DB
