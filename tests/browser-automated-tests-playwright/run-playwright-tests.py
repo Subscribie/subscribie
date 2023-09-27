ebd77aa2b5a084711f624bbe99cc3ec5a27f8735
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-09-27 22:22:14 +0100

    #1219 remove unecessery entries in the test DAG

--- tests/browser-automated-tests-playwright/run-playwright-tests.py
+++ tests/browser-automated-tests-playwright/run-playwright-tests.py
@@ -27,29 +27,23 @@ graph = {
         "@stripe_connect",
         "@475_shop_owner_create_free_trial",
         "@1005_shop_owner_terms_and_conditions_creation",
-        "@1219_shop-owner_enable_custom_url",
     ],
     "@264_subscriber_order_plan_with_choice_options_and_required_note": [
         "@stripe_connect",
         "@264_shop_owner_create_plan_with_choice_options",
-        "@1219_shop-owner_enable_custom_url",
     ],
     "@133_subscriber_order_plan_with_cooling_off_period": [
         "@stripe_connect",
         "@133_shop_owner_create_plan_with_cooling_off_period",
-        "@1219_shop-owner_enable_custom_url",
     ],
     "@293_shop_owner_order_recurring_plan": [
         "@stripe_connect",
-        "@1219_shop-owner_enable_custom_url",
     ],
     "@293-2_subscriber_order_plan_with_only_upfront_charge": [
         "@stripe_connect",
-        "@1219_shop-owner_enable_custom_url",
     ],
     "@293-3_subscriber_order_plan_with_recurring_and_upfront_charge": [
         "@stripe_connect",
-        "@1219_shop-owner_enable_custom_url",
     ],
     "@619_shop_owner_transaction_filter_by_name_and_by_plan_title": [
         "@stripe_connect",
@@ -70,7 +64,6 @@ graph = {
     "@939_subscriber_order_free_plan_with_terms_and_conditions": [
         "@1005_shop_owner_terms_and_conditions_creation",
         "@stripe_connect",
-        "@1219_shop-owner_enable_custom_url",
     ],
 }
 
