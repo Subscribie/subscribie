580a8dad518f92a3b834e13b17b1a824ad95d9a3
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-12-03 16:01:57 -0500

    finished updating faq page using css/style, html, and js

--- subscribie/themes/theme-builder/builder/faq.js
+++ subscribie/themes/theme-builder/builder/faq.js
@@ -13,7 +13,19 @@ colSm12.forEach((item, index) => {
         description.style.height = "0px";
         item.querySelector("i").classList.replace("fa-minus", "fa-plus");
       }
-    
+      removeOpen(index); //calling the function and also passing the index number of the clicked header
   })
   
 })
+
+function removeOpen(index){
+  colSm12.forEach((item2, index2) => {
+    if(index1 != index2){
+      item2.classList.remove("open");
+
+      let description = item2.querySelector(".description");
+      description.style.height = "0px";
+      item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+    }
+  })
+}
