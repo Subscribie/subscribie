697b51beb105f222ed0c8f730996d3bca6f0451c
chrisjsimpson <chris.j.simpson@live.co.uk>
2024-03-05 23:32:17 +0000

    #1308 if a subscription is cancelled by the lack of payments send shop owner notification (#1310)
    * Fix #1308 when a subscription is cancelled, email the shop owner with context
    
    * rm breakpoint

--- subscribie/blueprints/checkout/__init__.py
+++ subscribie/blueprints/checkout/__init__.py
@@ -829,7 +829,6 @@ def stripe_webhook():
     log.info(f"Received stripe webhook event type {event['type']}")
 
     if event["type"] == "customer.subscription.deleted":
-        breakpoint()
         eventObj = event["data"]["object"]
         cancellation_reason = eventObj["cancellation_details"]["reason"]
         if cancellation_reason == "payment_failed":
@@ -877,7 +876,7 @@ def stripe_webhook():
             ).one()
         except Exception:
             log.error(
-                "Unable to locate subscription associated with event customer.subscription.deleted"
+                "Unable to locate subscription associated with event customer.subscription.deleted"  # noqa: E501
             )
 
         if person is not None:
