86a223dab67919ed6fea733b8f1c2a8f40fdc96d
chrisjsimpson <chris.j.simpson@live.co.uk>
2024-05-21 23:18:57 +0100

    wip Fix #1333 Forms Questions during plan sign up

--- subscribie/blueprints/admin/choice_group.py
+++ subscribie/blueprints/admin/choice_group.py
@@ -1,7 +1,7 @@
 from . import admin
 from subscribie.auth import login_required
-from subscribie.forms import ChoiceGroupForm
-from subscribie.models import ChoiceGroup, Plan
+from subscribie.forms import ChoiceGroupForm, QuestionForm
+from subscribie.models import ChoiceGroup, Plan, Question, PlanQuestionAssociation
 from flask import request, render_template, url_for, flash, redirect
 from subscribie.database import database
 
@@ -25,8 +25,11 @@ def add_choice_group():
 @login_required
 def list_choice_groups():
     choice_groups = ChoiceGroup.query.all()
+    questions = Question.query.all()
     return render_template(
-        "admin/choice_group/list_choice_groups.html", choice_groups=choice_groups
+        "admin/choice_group/list_choice_groups.html",
+        choice_groups=choice_groups,
+        questions=questions,
     )
 
 
@@ -87,3 +90,143 @@ def delete_choice_group(id):
     database.session.commit()
     flash("Choice group deleted")
     return redirect(url_for("admin.list_choice_groups"))
+
+
+@admin.route("/list-questions", methods=["GET", "POST"])
+@login_required
+def list_questions():
+    questions = Question.query.all()
+    plans = Plan.query.all()
+
+    return render_template(
+        "admin/question/list_questions.html", questions=questions, plans=plans
+    )
+
+
+@admin.route("/add-question", methods=["GET", "POST"])
+@login_required
+def add_question():
+    form = QuestionForm()
+    if form.validate_on_submit():
+        question = Question()
+        question.title = request.form["title"]
+        database.session.add(question)
+        database.session.commit()
+        flash("Added new question")
+        return redirect(url_for("admin.list_questions"))
+
+    return render_template("admin/choice_group/add_question.html", form=form)
+
+
+@admin.route("/edit-question/<id>", methods=["GET", "POST"])
+@login_required
+def edit_question(id):
+    question = Question.query.get(id)
+    if request.method == "POST":
+        question.title = request.form["title"]
+        database.session.commit()
+        flash("Question updated")
+    return render_template("admin/question/edit_question.html", question=question)
+
+
+@admin.route("/question/<question_id>/assign-plan", methods=["GET", "POST"])
+@login_required
+def question_assign_plan(question_id):
+    question = Question.query.get(question_id)
+    plans = Plan.query.filter_by(archived=0)
+
+    if request.method == "POST":
+        # Remove question associations
+        for plan in plans:
+            database.session.query(PlanQuestionAssociation).filter(
+                PlanQuestionAssociation.plan_id == plan.id,
+                PlanQuestionAssociation.question_id == question.id,
+            ).delete()
+
+        # Add back only selected question/plan associations
+        for plan_id in request.form.getlist("assign"):
+            plan = Plan.query.get(plan_id)
+            plan_question_assoc = PlanQuestionAssociation()
+            plan_question_assoc.question_id = question.id
+            plan_question_assoc.plan_id = int(plan_id)
+            database.session.add(plan_question_assoc)
+
+        database.session.commit()
+        flash("The question assignments have been applied to selected plan(s)")
+        return redirect(url_for("admin.list_questions"))
+
+    return render_template(
+        "admin/question/question_assign_plan.html",
+        question=question,
+        plans=plans,
+    )
+
+
+@admin.route("/delete-question/<id>", methods=["GET"])
+@login_required
+def delete_question(id):
+    if "confirm" in request.args:
+        questions = Question.query.all()
+
+        return render_template(
+            "admin/question/list_questions.html",
+            questions=questions,
+            question=Question.query.get(id),
+            is_question_delete_request=True,
+            confirm=False,
+        )
+    breakpoint()
+    question = Question.query.get(id)
+
+    # Delete PlanQuestionAssociation with that question:
+    database.session.query(PlanQuestionAssociation).filter(
+        PlanQuestionAssociation.question_id == id
+    ).delete()
+
+    # Delete the question itself:
+    database.session.delete(question)
+    database.session.commit()
+
+    flash("Question deleted")
+    return redirect(url_for("admin.list_questions"))
+
+
+@admin.route("/plan/questions/set-question-order/<plan_id>", methods=["GET", "POST"])
+@login_required
+def set_question_order_by_plan(plan_id):
+    plan = Plan.query.get(plan_id)
+
+    if request.method == "POST":
+        """
+        For each question passed, get & and save the chosen order for
+        the appearance order
+        """
+        for plan_question_assoc_id in request.form.getlist("plan_question_assoc_id"):
+            order_value = int(
+                request.form.get(
+                    f"order-value-for-question_assoc-id-{plan_question_assoc_id}"
+                )
+            )
+            question_id = int(
+                request.form.get(
+                    f"question_id-for-question_assoc-id-{plan_question_assoc_id}"
+                )
+            )
+            # Get by composite key (question_id, plan_id)
+            plan_question_association = PlanQuestionAssociation.query.get(
+                (question_id, plan.id)
+            )
+            plan_question_association.order = order_value
+            database.session.add(plan_question_association)
+
+        database.session.commit()
+        flash("The plans question's have been ordered")
+        return redirect(url_for("admin.set_question_order_by_plan", plan_id=plan.id))
+
+    # Sort questions by order
+    plan.questions.sort(key=lambda question: question.order or 0)
+
+    return render_template(
+        "admin/question/set_question_order.html",
+        plan=plan,
+    )
--- subscribie/blueprints/admin/export_subscribers.py
+++ subscribie/blueprints/admin/export_subscribers.py
@@ -3,6 +3,7 @@ from subscribie.auth import login_required
 from subscribie.models import Subscription
 from flask import request, Response, jsonify
 import logging
+import json
 
 
 @admin.route("/export-subscribers-email")
@@ -15,15 +16,20 @@ def export_subscribers():
     subscribers = []
     for subscription in subscriptions:
         if subscription.person is not None:
-            subscribers.append(
-                {
-                    "given_name": subscription.person.given_name,
-                    "family_name": subscription.person.family_name,
-                    "email": subscription.person.email,
-                    "plan": subscription.plan.title,
-                    "subscription_status": subscription.stripe_status,
-                }
-            )
+            person = {
+                "given_name": subscription.person.given_name,
+                "family_name": subscription.person.family_name,
+                "email": subscription.person.email,
+                "plan": subscription.plan.title,
+                "subscription_status": subscription.stripe_status,
+            }
+            # Include subscription question answers in export, if any
+            if subscription.question_answers:
+                for answer in subscription.question_answers:
+                    person[
+                        f"question_id-{answer.question_id}-{json.dumps(answer.question_title)}"  # noqa: E501
+                    ] = json.dumps(answer.response)
+            subscribers.append(person)
         else:
             logging.info(
                 f"Excluding subscription {subscription.id} as no person attached"
@@ -34,7 +40,7 @@ def export_subscribers():
         import io
 
         outfile = io.StringIO()
-        outcsv = csv.DictWriter(outfile, fieldnames=subscribers[0].keys())
+        outcsv = csv.DictWriter(outfile, fieldnames=subscribers[0].keys(), extrasaction='ignore')
         outcsv.writeheader()
         for subscriber in subscribers:
             outcsv.writerow(subscriber)
