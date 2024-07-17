e5abd7feebbc9016df47e423ab4f7f9aa4dfa2de
chrisjsimpson <chris.j.simpson@live.co.uk>
2024-07-16 21:25:08 -0700

    1376 as a shop owner i can view all invoices of a specific subscriber and refresh individual subscription statuses (#1378)
    * Fix #1376 As a shop owner, I can download invoices when I view an individual subscriber
    
    * #1376 black format
    
    * #1376 as a shop owner I can refresh individual subscriptions where the transaction count is > 1
    
    * #1374 refresh_subscription login required
    
    * #1376 As a shop owner I can refresh individual subscribers subscription statuses
    
    * #1374 refresh_subscription login required
    
    * #1376 As a shop owner I can refresh individual subscribers subscription statuses
    
    * #1376 format black

--- subscribie/blueprints/admin/__init__.py
+++ subscribie/blueprints/admin/__init__.py
@@ -78,7 +78,10 @@ from subscribie.models import (
     Document,
     PlanQuestionAssociation,
 )
-from .subscription import update_stripe_subscription_statuses
+from .subscription import (
+    update_stripe_subscription_statuses,
+    update_stripe_subscription_status,
+)
 from .stats import (
     get_number_of_active_subscribers,
     get_number_of_subscribers,
@@ -1356,6 +1359,27 @@ def show_recent_subscription_cancellations():
     return render_template(
         "admin/recent_subscription_cancellations.html",
         cancellations=cancellations,
+    )
+
+
+@admin.route("/refresh-subscription-status/<subscription_uuid>", methods=["GET"])
+@admin.route(
+    "/refresh-subscription-status/<subscription_uuid>/<person_id>", methods=["GET"]
+)
+@login_required
+def refresh_subscription(subscription_uuid: str, person_id=None):
+    update_stripe_subscription_status(subscription_uuid)
+    flash("The subscription status has been force refreshed")
+    if person_id is not None:
+        return redirect(url_for("admin.show_subscriber", subscriber_id=person_id))
+    # Redirect directly to scroll to the subscriber just refreshed:
+    subscription = (
+        database.session.query(Subscription)
+        .where(Subscription.uuid == subscription_uuid)
+        .first()
+    )
+    return redirect(
+        url_for("admin.subscribers", _anchor=f"person-{subscription.person.uuid}")
     )
 
 
