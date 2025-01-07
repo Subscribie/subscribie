99900ef16d045ae9bf3de4ee5f7ceba0652f69bf
chrisjsimpson <chris.j.simpson@live.co.uk>
2025-01-07 00:01:27 +0000

    #1431 correct flakey tests, increase fetch upcoming invoices wait time (to give time for invoices to be created at stripes end

--- tests/browser-automated-tests-playwright/e2e/features/fetch_upcomming_invoices.js
+++ tests/browser-automated-tests-playwright/e2e/features/fetch_upcomming_invoices.js
@@ -1,9 +1,10 @@
 var fetch_upcomming_invoices = async function (page) {
     // Go to upcoming payments and ensure plan is attached to upcoming invoice
     await page.goto('/admin/upcoming-invoices');
-    await new Promise(x => setTimeout(x, 25000));
+    await new Promise(x => setTimeout(x, 35000));
     await page.click('#fetch_upcoming_invoices');
-    await new Promise(x => setTimeout(x, 20000));
+    await new Promise(x => setTimeout(x, 30000));
+    await page.reload();
 }
 
 module.exports.fetch_upcomming_invoices = fetch_upcomming_invoices;
