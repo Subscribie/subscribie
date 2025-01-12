4b7588b56421df019db92da5d7759732eb57a213
chrisjsimpson <chris.j.simpson@live.co.uk>
2025-01-12 12:56:37 +0000

    #1431 add test @1431_shop_owner_bulk_pause_payment_collection_all_subscribers

---
+++ tests/browser-automated-tests-playwright/e2e/1431_shop_owner_bulk_pause_payment_collection_all_subscribers.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+const { set_test_name_cookie } = require('./features/set_test_name_cookie');
+const { admin_login } = require('./features/admin_login');
+
+test('test', async ({ page }) => {
+  await admin_login(page);
+  await set_test_name_cookie(page, "@1431_shop_owner_bulk_pause_payment_collection_all_subscribers");
+  await page.goto('http://127.0.0.1:5000/admin/dashboard');
+  await page.getByRole('button', { name: 'My Subscribers' }).click();
+  await page.getByRole('link', { name: 'Pause all Subscribers payment' }).click();
+  await page.getByRole('link', { name: 'Pause payment collection for' }).click();
+  await page.getByRole('link', { name: 'Yes' }).click();
+  await page.getByText('All payment collections are').click();
+  await expect(page.getByText('All payment collections are being paused in the background. You can move away from this page.')).toBeVisible();
+  await page.goto('http://127.0.0.1:5000/admin/dashboard');
+  await page.getByRole('button', { name: 'My Subscribers' }).click();
+  await page.getByRole('link', { name: 'View Subscribers' }).click();
+  await page.getByRole('button', { name: 'Refresh Subscriptions' }).click();
+  await page.waitForTimeout(3000);
+  await page.reload();
+  await page.getByText('Paused - (keep_as_draft)').first().click();
+  await page.getByText('Paused - (keep_as_draft)').nth(1).click();
+});
